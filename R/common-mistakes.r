f283fb7296bd4fee89f38ef7dcd10bd297a34538
hadley <h.wickham@gmail.com>
2010-05-03 09:01:15 -0500

    Documentation for common mistakes

--- R/common-mistakes.r
+++ R/common-mistakes.r
@@ -1,28 +1,38 @@
+#' Trace multiple functions with same tracer.
+#'
+#' @keywords programming
+#' @export 
+#' @examples
+#' trace_all(
+#'   functions_with_arg("na.rm", "package:base"), 
+#'   quote(if (!na.rm) warning("na.rm = FALSE"))
+#' )
+#' trace_all(
+#'   functions_with_arg("drop", "package:base"), 
+#'   quote(if (drop) warning("drop = TRUE"))
+#' )
 trace_all <- function(fs, tracer) {
-  lapply(fs, trace, exit = tracer, print=FALSE)
-  invisible()
+  l_ply(fs, trace, exit = tracer, print = FALSE)
 }
 
+#' Find all functions with specified argument.
+#'
+#' @keywords programming
+#' @export
+#' @examples
+#' functions_with_arg("drop", "package:base")
+#' functions_with_arg("na.rm", "package:base")
 functions_with_arg <- function(arg, pos) {
   fs <- ls(pos=pos)
-  present <- unlist(lapply(fs, function(x) 
-    (is.function(get(x)) || is.primitive(get(x))) &&  !is.null(formals(x)[[arg]])))
-
-  fs[present]
+  
+  has_arg <- function(f) {
+    (is.function(f) || is.primitive(f)) && !is.null(formals(f)[[arg]])
+  }
+  
+  Filter(function(x) has_arg(get(x)), fs)
 }
-# 
-# trace_all(
-#   functions_with_arg("drop", "package:base"), 
-#   quote(if (drop) warning("na.rm = FALSE"))
-# )
-# 
-# 
-# #trace_all(list("sum"), quote(if (!na.rm) warning("na.rm = FALSE")))
-# 
-# trace_all(
-#   functions_with_arg("drop", "package:base"), 
-#   quote(if (drop) warning("drop = TRUE"))
-# )
-# 
+
+
+#' Force full specification of logicals
 # makeActiveBinding("T", function(value) stop("Use TRUE!", call. = FALSE), globalenv())
 # makeActiveBinding("F", function(value) stop("Use FALSE!", call. = FALSE), globalenv())
