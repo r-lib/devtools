cdb670b04dedfcc11d7c3cf406933615429a89a1
hadley <h.wickham@gmail.com>
2010-05-02 22:08:20 -0500

    Check for data directory first

--- R/data.r
+++ R/data.r
@@ -2,6 +2,7 @@ load_data <- function(pkg) {
   pkg <- as.package(pkg)
   
   path_data <- file.path(pkg$path, "data")
+  if (!file.exists(path_data)) return(invisible())
   
   paths <- dir(path_data, "\\.[rR]da(ta)?$", full = TRUE)
 
