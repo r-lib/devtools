6a7141f3f8da9eedabae7c345ab45404c7986b2b
hadley <h.wickham@gmail.com>
2010-05-03 08:58:05 -0500

    Better handling of package global

--- R/file-cache.r
+++ R/file-cache.r
@@ -1,4 +1,3 @@
-.file_cache <- character()
 
 #' Generate checksums for a vector of file paths.
 #' @keywords internal
@@ -9,16 +8,21 @@ md5 <- function(paths) {
 #' Given vector of paths, return only those paths that have changed since the
 #' last invocation.
 #' @keywords internal
-changed_files <- function(paths) {
-  paths <- path.expand(paths)
-  new_hash <- md5(paths)
-  old_hash <- .file_cache[paths]
+make_cache <- function() {
+  .file_cache <- character()
+
+  function(paths) {
+    paths <- path.expand(paths)
+    new_hash <- md5(paths)
+    old_hash <- .file_cache[paths]
   
-  changed <- is.na(old_hash) | new_hash != old_hash
-  .file_cache[paths[changed]] <<- new_hash[changed]
+    changed <- is.na(old_hash) | new_hash != old_hash
+    .file_cache[paths[changed]] <<- new_hash[changed]
   
-  paths[changed]
+    paths[changed]
+  }
 }
+changed_files <- make_cache()
 
 #' Clear file cache.
 #' @keywords internal
