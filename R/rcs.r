aba1fbf90519888e3c9aa87c942daefc8778f685
hadley <h.wickham@gmail.com>
2010-05-12 14:55:29 -0500

    Update package source code with update()

---
+++ R/rcs.r
@@ -0,0 +1,35 @@
+#' Update the source code of a package
+#'
+#' @param pkg package description, can be path or package name.  See
+#'   \code{\link{as.package}} for more information
+#' @export
+update <- function(pkg) {
+  pkg <- as.package(pkg)
+  
+  cmd <- str_c("cd ", pkg$path, " && ")
+  
+  if (is.svn(pkg)) {
+    cmd <- str_c(cmd, "svn up")
+  } else if (is.git(pkg)) {
+    cmd <- str_c(cmd, "git pull")    
+  } else {
+    stop("Unknown rcs software")
+  }
+  
+  system(cmd)
+}
+
+
+#' Does this package use svn?
+#' @keywords internal
+is.svn <- function(pkg) {
+  pkg <- as.package(pkg)
+  file.exists(file.path(pkg$path, ".svn"))
+}
+
+#' Does this package use git?
+#' @keywords internal
+is.git <- function(pkg) {
+  pkg <- as.package(pkg)
+  file.exists(file.path(pkg$path, ".git"))
+}
