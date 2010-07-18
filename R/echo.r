534cecd4b8002314ae40840beb45b7d57149e3ce
hadley <h.wickham@gmail.com>
2010-07-18 18:35:55 -0500

    Improvements to echo (should be replaced by evaluate

--- R/echo.r
+++ R/echo.r
@@ -5,8 +5,6 @@ eval.echo <- function (exprs, envir = parent.frame()) {
   eval.with.vis <- function(expr, envir = parent.frame(), enclos = if (is.list(envir) || is.pairlist(envir)) parent.frame() else baseenv()) 
   .Internal(eval.with.vis(expr, envir, enclos))
     
-  Ne <- length(exprs)
-
   sd <- "\""
   nos <- "[^\"]*"
   oddsd <- paste("^", nos, sd, "(", nos, sd, nos, sd, ")*", 
@@ -14,27 +12,28 @@ eval.echo <- function (exprs, envir = parent.frame()) {
 
   srcrefs <- attr(exprs, "srcref")
   srcfile <- attr(exprs, "srcfile")
-  for (i in 1L:Ne) {
+  yy <- NULL
+  for (i in seq_along(exprs)) {
     ei <- exprs[i]
     srcref <- srcrefs[[i]]
     if (i == 1) 
       lastshown <- min(0, srcref[3L] - 1)
-    dep <- getSrcLines(srcfile, lastshown + 1, srcref[3L])
+    src <- getSrcLines(srcfile, lastshown + 1, srcref[3L])
     leading <- srcref[1L] - lastshown
     lastshown <- srcref[3L]
-    while (length(dep) && length(grep("^[[:blank:]]*$", 
-      dep[1L]))) {
-      dep <- dep[-1L]
+    while (length(src) && length(grep("^[[:blank:]]*$", 
+      src[1L]))) {
+      src <- src[-1L]
       leading <- leading - 1L
     }
-    dep <- paste(rep.int(c(prompt.echo, continue.echo), 
-      c(leading, length(dep) - leading)), dep, sep = "", 
+    src <- paste(rep.int(c(prompt.echo, continue.echo), 
+      c(leading, length(src) - leading)), src, sep = "", 
       collapse = "\n")
-    nd <- nchar(dep, "c")
 
+    nd <- nchar(src, "c")
     if (nd) {
-      dep <- substr(dep, 1L, nd)
-      cat("\n", dep, "\n", sep = "")
+      src <- substr(src, 1L, nd)
+      cat(src, "\n", sep = "")
     }
     yy <- eval.with.vis(ei, envir)
     i.symbol <- mode(ei[[1L]]) == "name"
