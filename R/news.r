115f1047fa614f7d4283f89bcae71be0ca8272c4
hadley <h.wickham@gmail.com>
2010-09-01 10:51:56 -0500

    show_news to display package news

---
+++ R/news.r
@@ -0,0 +1,9 @@
+#' Show package news
+#' 
+#' @param pkg package description, can be path or package name.  See
+#'   \code{\link{as.package}} for more information
+show_news <- function(pkg, ...) {
+  pkg <- as.package(pkg)
+  news_path <- file.path(pkg$path, "NEWS")
+  news(..., db = tools:::.news_reader_default(news_path))
+}
