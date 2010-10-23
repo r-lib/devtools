0d8e3f4499a59c3848c877eb1c32cecd2433fecd
hadley <h.wickham@gmail.com>
2010-10-23 13:28:10 -0500

    Export show news

--- R/news.r
+++ R/news.r
@@ -2,6 +2,7 @@
 #' 
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
+#' @export
 show_news <- function(pkg, ...) {
   pkg <- as.package(pkg)
   news_path <- file.path(pkg$path, "NEWS")
