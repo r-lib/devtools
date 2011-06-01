9ae87a84ff7ad9541377157114196dfa4fff044c
hadley <h.wickham@gmail.com>
2011-06-01 08:33:53 -0500

    Better error message if news missing

--- R/news.r
+++ R/news.r
@@ -10,6 +10,10 @@ show_news <- function(pkg, latest = TRUE, ...) {
   pkg <- as.package(pkg)
   news_path <- file.path(pkg$path, "NEWS")
   
+  if (!file.exists(news_path)) {
+    stop("No NEWS found", call. = FALSE)
+  }
+  
   out <- news(..., db = tools:::.news_reader_default(news_path))
   if (latest) {
     ver <- numeric_version(out$Version)
