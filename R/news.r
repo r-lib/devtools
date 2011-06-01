3488889cd5bc848cb911486bdcfe5ff28e864230
hadley <h.wickham@gmail.com>
2011-06-01 08:28:58 -0500

    By default, only show latest news

--- R/news.r
+++ R/news.r
@@ -2,9 +2,21 @@
 #' 
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
+#' @param latest if \code{TRUE}, only show the news for the most recent 
+#'   version.
+#' @param ... other arguments passed on to \code{news}
 #' @export
-show_news <- function(pkg, ...) {
+show_news <- function(pkg, latest = TRUE, ...) {
   pkg <- as.package(pkg)
   news_path <- file.path(pkg$path, "NEWS")
-  news(..., db = tools:::.news_reader_default(news_path))
+  
+  out <- news(..., db = tools:::.news_reader_default(news_path))
+  if (latest) {
+    ver <- numeric_version(out$Version)
+    recent <- ver == max(ver)
+    structure(out[recent, ], class = class(out),
+      bad = attr(out, "bad")[recent])
+  } else {
+    out
+  }
 }
