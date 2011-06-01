4f98abcf4561a1385592cdf8d81e86f3f188a21e
hadley <h.wickham@gmail.com>
2011-06-01 10:51:39 -0500

    Basic interface to R CMD check

---
+++ R/check.r
@@ -0,0 +1,22 @@
+#' Build and check a package, cleaning up automatically on success.
+#'
+#' @param pkg package description, can be path or package name.  See
+#'   \code{\link{as.package}} for more information
+#' @export
+check <- function(pkg) {
+  pkg <- as.package(pkg)
+  
+  in_dir(dirname(pkg$path), {
+    targz <- paste(pkg$package, "_", pkg$version, ".tar.gz", sep = "")
+    
+    system(paste("R CMD build ", basename(pkg$path), sep = ""))
+    res <- system(paste("R CMD check ", targz, sep = ""))
+    
+    if (res != 0) {
+      stop("R CMD check ", pkg$package, " failed", call. = FALSE, sep = "")
+    }
+    
+    unlink(targz)
+    unlink(paste(pkg$package, ".Rcheck", sep = ""))
+  })  
+}
