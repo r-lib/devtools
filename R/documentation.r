27d62cc87a8b24e7c5831d34e9feb740ef7d667b
hadley <h.wickham@gmail.com>
2011-06-12 18:32:37 -0700

    Automatically check after roxygenise

--- R/documentation.r
+++ R/documentation.r
@@ -2,9 +2,12 @@
 #'
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
+#' @param check if \code{TRUE} (the default), will automatically 
+#'   check documentation after running roxygen.
 #' @keywords programming
 #' @export
-document <- function(pkg) {
+document <- function(pkg, check = TRUE) {
+  message("Updating documentation with roxygen")
   pkg <- as.package(pkg)
   
   require(roxygen)
@@ -16,4 +19,27 @@ document <- function(pkg) {
     roxygenize(pkg$path, pkg$path)
   }
   
+  if (check) check_doc(pkg)
+  invisible()
+}
+
+check_doc <- function(pkg) {
+  message("Checking documentation")
+  pkg <- as.package(pkg)
+
+  check <- tools:::.check_package_description(
+    file.path(pkg$path, "DESCRIPTION"))
+  if (length(check) > 0) {
+    msg <- capture.output(tools:::print.check_package_description(check))
+    message("Invalid DESCRIPTION:\n", paste(msg, collapse = "\n"))
+  }
+  
+  path_man <- file.path(pkg$path, "man")
+  files <- dir(path_man, pattern = "\\.[Rr]d$", full = TRUE)
+  files <- sort(files)
+  
+  lapply(files, function(x) {
+    print(tools::checkRd(x))
+  })
+  invisible()
 }
