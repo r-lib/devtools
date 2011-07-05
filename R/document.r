a4cb05c6274b520bc93a8d1924e4d0db61871321
hadley <h.wickham@gmail.com>
2011-07-05 13:27:15 -0500

    New clean parameter for document

--- R/document.r
+++ R/document.r
@@ -2,11 +2,13 @@
 #'
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
-#' @param check if \code{TRUE} (the default), will automatically 
+#' @param clean if \code{TRUE} will automatically clear all roxygen caches
+#'   and delete currently man contents to ensure that you have the freshest
+#'   version of the documentation.
 #'   check documentation after running roxygen.
 #' @keywords programming
 #' @export
-document <- function(pkg = NULL, check = TRUE) {
+document <- function(pkg = NULL, clean = FALSE) {
   roxygen_installed <- length(find.package("roxygen", quiet = T)) > 0
   
   if (!roxygen_installed || packageVersion("roxygen") < 1) {
@@ -16,6 +18,11 @@ document <- function(pkg = NULL, check = TRUE) {
   
   pkg <- as.package(pkg)
   message("Updating ", pkg$package, " documentation")
+  
+  if (clean) {
+    clear_caches()
+    file.remove(dir(file.path(pkg$path, "man"), full = TRUE))
+  }
   
   in_dir(pkg$path, roxygenise("."))
     
