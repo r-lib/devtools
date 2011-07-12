f1b144941ab2593fe960b283816aef4a8b89b492
hadley <h.wickham@gmail.com>
2011-07-12 08:39:32 -0500

    First pass at onload path checking for windows

---
+++ R/zzz.r
@@ -0,0 +1,19 @@
+.onLoad <- function() {
+  if (!os() == "win") return()
+  
+  rtools <- normalizePaths("c:\\Rtools\\bin", mustWork = FALSE)
+  if (!file.exists(rtools)) {
+    message("Rtools not installed.")
+    return()
+  }
+
+  paths <- strsplit(Sys.getenv("PATH"), ";")[[1]]
+  paths <- normalizePaths(paths, mustWork = FALSE)
+
+  in_path <- any(paths == rtools)
+  if (!in_path) {
+    message("Rtools not in path, adding automatically.")
+    path <- paste(c(rtools, paths), collapse = ";")
+    Sys.setenv(PATH = path)
+  }
+}
