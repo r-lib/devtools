584244ed331b6a9987f05494033103c3030b57de
hadley <h.wickham@gmail.com>
2012-01-02 11:54:06 -0600

    Function to detect missing s3 exports

---
+++ R/missing-s3.r
@@ -0,0 +1,19 @@
+#' Find missing s3 exports.
+#'
+#' The method is heuristic - looking for objs with a period in their name.
+#'
+#' @param pkg package description, can be path or package name.  See
+#'   \code{\link{as.package}} for more information
+#' @export
+missing_s3 <- function(pkg) {
+  pkg <- as.package(pkg)
+  ns <- parseNamespaceFile(basename(pkg$path), dirname(pkg$path))
+
+  exports <- paste(ns$S3methods[, 1], ns$S3methods[, 2], sep = ".")
+
+  load_all(pkg)
+  objs <- ls(env_name(pkg))
+  s3_objs <- objs[grepl(".", objs, fixed = TRUE)]
+
+  setdiff(s3_objs, exports)
+}
