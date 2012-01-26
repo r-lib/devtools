7b64d4de74dd88c40fb3515e9f3471c4ac125d98
Jeremy Stephens <viking@pillageandplunder.net>
2012-01-26 15:38:20 -0600

    Add as.of.date parameter to install_version()

--- R/install-version.r
+++ R/install-version.r
@@ -11,20 +11,40 @@
 #'   recent version of the package, this function simply calls
 #'   \code{\link{install.packages}}. Otherwise, it looks at the list of
 #'   archived source tarballs and tries to install an older version instead.
+#' @param as.of.date If the specified date is not NULL and version is NULL,
+#'   attempt to find the package version as of a certain date.
 #' @inheritParams utils::install.packages
 #' @author Jeremy Stephens
-install_version <- function(package, version = NULL, repos = getOption("repos"), type = getOption("pkgType")) {
+install_version <- function(package, version = NULL, as.of.date = NULL, repos = getOption("repos"), type = getOption("pkgType")) {
   
   contriburl <- contrib.url(repos, type)
   available <- available.packages(contriburl)
   
   if (!is.null(version)) {
     version <- numeric_version(version)
+
+    # always override date with version
+    as.of.date <- NULL
   }
-  
+  else if (!is.null(as.of.date)) {
+    as.of.date <- as.Date(as.of.date)
+  }
+
   if (package %in% row.names(available)) {
-    current.version <- available[package, 'Version']
-    if (is.null(version) || version == current.version) {
+    if (!is.null(as.of.date)) {
+      dcon <- url(sprintf("%s/web/packages/%s/DESCRIPTION", repos, package))
+      on.exit(close(dcon))
+      dcf <- read.dcf(dcon)
+
+      current.release.date <- as.Date(dcf[1, 'Date'])
+      install.latest <- (as.of.date > current.release.date)
+    }
+    else {
+      current.version <- available[package, 'Version']
+      install.latest <- (is.null(version) || version == current.version)
+    }
+
+    if (install.latest) {
       return(install.packages(package, repos = repos, contriburl = contriburl,
         type = type))
     }
@@ -39,16 +59,46 @@ install_version <- function(package, version = NULL, repos = getOption("repos"),
     stop(sprintf("couldn't find package '%s'", package))
   }
 
-  if (is.null(version)) {
-    # Grab the latest one: only happens if pulled from CRAN
-    package.path <- info[length(info)]
-  } else {
+  if (!is.null(version)) {
     package.path <- paste(package, "/", package, "_", version, ".tar.gz",
       sep = "")
     if (!(package.path %in% info)) {
-      stop(sprintf("version '%s' is invalid for package '%s'", package,
-        version))
+      stop(sprintf("version '%s' is invalid for package '%s'", version,
+        package))
     }
+  }
+  else if (!is.null(as.of.date)) {
+    # This depends on the modification time of files in the archive. The
+    # alternative is to untar each file and check the DESCRIPTION file.
+
+    package.path <- NULL
+    for (i in length(info):1) {
+      # Grab only the HEAD of the file (using RCurl)
+      g <- basicTextGatherer()
+      curl <- getCurlHandle()
+      url <- paste(repos, "/src/contrib/Archive/", info[i], sep = "")
+
+      curlPerform(url = url, followlocation = TRUE,
+        headerfunction = g$update, nobody = TRUE, writefunction = g$update,
+        curl = curl)
+      header <- parseHTTPHeader(g$value())
+
+      release.date <- as.Date(header['Last-Modified'], format = "%a, %d %b %Y")
+      if (as.of.date >= release.date) {
+        package.path <- info[i]
+        break
+      }
+    }
+    print(package.path)
+
+    if (is.null(package.path)) {
+      stop(sprintf("package '%s' didn't exist as of '%s'", package,
+        as.of.date))
+    }
+  }
+  else {
+    # Grab the latest one: only happens if pulled from CRAN
+    package.path <- info[length(info)]
   }
 
   url <- paste(repos, "/src/contrib/Archive/", package.path, sep = "")  
