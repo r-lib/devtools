b4bf24b14b6b0004992244d376f1612111d97825
Phil Chalmers <rphilip.chalmers@gmail.com>
2012-04-25 19:17:13 -0400

    normalizePath() used

--- R/cd.r
+++ R/cd.r
@@ -3,33 +3,24 @@
 #' \code{cd} changes the current working directory similar to how a 
 #' bash shell changes relative directories. 
 #'
-#' @param dir a string indicating where to change the directory to.
+#' @param dir a string indicating the relative or absolute directory to be set.
 #' If \code{dir = '..'} then the working directory is moved up the directory
-#' chain, otherwise the directory is moved to a lower directory name. If  
-#' \code{dir} begins with \code{/} (unix) or contains a \code{:} (windows) 
-#' then \code{setwd()} is invoked.
+#' path, otherwise the directory is moved to a lower or absolute directory. 
 #' @param levels number of levels that can be moved up when \code{dir='..'}.
 #' Default is to move 1 level up.
 #' @export
 cd <- function(dir, levels = 1){
-    if(substr(dir, 1, 1) == '/' || substr(dir, 1, 1) == '~'){ 
+    dir <- normalizePath(dir, winslash='/')
+    if(substr(dir, 1, 1) == '/' || substr(dir, 1, 1) == '~' || substr(dir, 2, 3) == ':/'){ 
         setwd(dir)
-        return(invisible(''))
-    }
-    if(nchar(dir) > 2){
-        if(substr(dir, 2, 3) == ':/' || substr(dir, 2, 3) == ":\\"  ){ 
-            setwd(dir)
-            return(invisible(''))
-        }
+        return(invisible())
     }    
-    old <- getwd() 
-    if(substr(old, nchar(old), nchar(old)) != '/') 
-        old <- paste(old, '/', sep="")
+    old <- paste(normalizePath(getwd(), winslash='/'), '/', sep = '')     
     if(dir == '..'){
         new <- old
         for(i in 1:levels){
             forward_slash <- gregexpr('/', new)[[1]]
-            new <- substr(new, 1, forward_slash[length(forward_slash)-1])            
+            new <- substr(new, 1, forward_slash[length(forward_slash) - 1])            
         }        
     } else {
         new <- paste(old, dir, sep='')    
