a4ef0b9d6ed7f1377cda3f41b13c036f74a605f0
Andrew Redd <amredd@gmail.com>
2012-05-09 11:18:53 -0600

    Documentation updates for system path manipulations.

--- R/path.r
+++ R/path.r
@@ -1,5 +1,7 @@
-#' get the PATH variable as a vector 
+#' Get the PATH variable as a vector.
+#' 
 #' @export
+#' @family path
 #' @examples
 #' path <- get_path()
 #' length(path)
@@ -7,22 +9,29 @@ get_path <- function() {
   strsplit(Sys.getenv("PATH"),.Platform$path.sep)[[1]]
 }
 
-#' set the path from a vector of individual paths
+#' Set the path from a vector of individual paths.
+#'
 #' @param path character vector of locations
-#' @note This only works for the current session.  The path is not set permanantly.
+#' @note This only works for the current session.  The path is not set permanently.
+#' @return invisibly returns the old path.
+#' @family path
 #' @export
 set_path <- function(path) {
+  old.path <- get_path()
   sep  <- .Platform$path.sep
   path <- paste(normalizePath(path), sep = sep, collapse = sep)
   Sys.setenv(PATH = path)
+  return(invisible(old.path))
 }
 
-#' Add a location to the Path variable
+#' Add a location to the Path variable.
 #' 
 #' Temporarily adds a location to the path.
 #' 
 #' @param ... character locations
 #' @param after the place on the path, defaults to the end
+#' @family path
+#' @return invisibly returns the old path.
 #' @export
 #'
 add_path <- function(..., after = Inf) {
@@ -31,9 +40,15 @@ add_path <- function(..., after = Inf) {
 }
 
 
-#' test if an object is on the path
-#' @param ... Strings indicating the exacutables to check for on the path.
+#' Test if an object is on the path.
+#'
+#' @param ... Strings indicating the executables to check for on the path.
+#' @family path
 #' @export
+#' @examples
+#' on_path("R")
+#' on_path("gcc")
+#' on_path("foo", "bar")  # FALSE in most cases
 on_path <- function(...) {
  unname(Sys.which(as.character(c(...))) != "")
 }
--- R/zzz.r
+++ R/zzz.r
@@ -17,20 +17,20 @@
       "Rtools not on path and not installed in default location.")
     return()
   } else {
-    rtools <- normalizePath(file.path(rtools.home,'bin'), mustWork=FALSE)
+    rtools <- normalizePath(file.path(rtools.home, 'bin'), mustWork=FALSE)
   }
   
   if(key$`Current Version`=="2.15"){ 
-    gccbin <- normalizePath(file.path(rtools.home,'gcc-4.6.3','bin'), mustWork=FALSE)
-    if(!(gccbin %in% get_path()) && file.exists(gccbin)) try(add_path(gccbin, after=0))
-    if(!(rtools %in% get_path()) && file.exists(rtools)) try(add_path(rtools, after=0))
+    gccbin <- normalizePath(file.path(rtools.home, 'gcc-4.6.3', 'bin'), mustWork=FALSE)
+    if(!(gccbin %in% get_path()) && file.exists(gccbin)) try(add_path(gccbin, after = 0))
+    if(!(rtools %in% get_path()) && file.exists(rtools)) try(add_path(rtools, after = 0))
   } else if (!(rtools %in% get_path())) {
     packageStartupMessage("Rtools not in path, adding automatically.")
-    gccbin <- normalizePath(file.path(rtools.home, "MinGW","bin"), mustWork=FALSE)
-    if(!(gccbin %in% get_path()) && file.exists(gccbin)) try(add_path(gccbin, after=0))
-    if(!(rtools %in% get_path()) && file.exists(rtools)) try(add_path(rtools, after=0))
+    gccbin <- normalizePath(file.path(rtools.home, "MinGW", "bin"), mustWork=FALSE)
+    if(!(gccbin %in% get_path()) && file.exists(gccbin)) try(add_path(gccbin, after = 0))
+    if(!(rtools %in% get_path()) && file.exists(rtools)) try(add_path(rtools, after = 0))
     if (.Platform$r_arch == "x64")
-      gcc.64 <- normalizePath(file.path(rtools.home, "MinGW64","bin"), mustWork=FALSE)
-      if(!(gcc.64 %in% get_path()) && file.exists(gcc.64)) try(add_path(gcc.64, after=0))
+      gcc.64 <- normalizePath(file.path(rtools.home, "MinGW64", "bin"), mustWork=FALSE)
+      if(!(gcc.64 %in% get_path()) && file.exists(gcc.64)) try(add_path(gcc.64, after = 0))
   }
 }
