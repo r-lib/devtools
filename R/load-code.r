5f6b0dc02e996ab07fd1789fcce1a4b6eef5f248
hadley <h.wickham@gmail.com>
2012-07-02 08:08:32 -0700

    Load codes files ending in .q

--- R/load-code.r
+++ R/load-code.r
@@ -65,7 +65,7 @@ parse_collate <- function(string) {
 #' Find all R files in given directory.
 #' @keywords internal
 find_code <- function(path) {
-  code_paths <- dir(path, "\\.[Rr]$", full.names = TRUE)  
+  code_paths <- dir(path, "\\.[Rrq]$", full.names = TRUE)  
   with_collate("C", sort(code_paths))
 }
 
