333545f3bcb5144dfbf2bec5c3bc3dfe07e55807
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2012-07-26 12:43:45 -0400

    Add default package-level documentation.
    Add dependency on 'brew', a templating library.

---
+++ R/create-package-doc.r
@@ -0,0 +1,14 @@
+#' Creates package-level documentation.
+#' @param path path to the package
+#' @param name of the package
+#' @importFrom brew brew
+#' @export
+create_package_doc <- function(path, name) {
+  
+  package_doc_file <- file.path(path, 'R', sprintf("%s-package.r", name))
+  brew(
+       file=(system.file("templates/packagename-package.r", package="devtools")),
+       output=package_doc_file
+       )
+
+}
---
+++ inst/templates/packagename-package.r
@@ -0,0 +1,8 @@
+#' <%= name %>
+#' 
+#' @name <%= name %>-package
+#' @aliases <%= name %>
+#' @docType package
+#' @title <%= name %>
+#' @keywords package
+NULL
