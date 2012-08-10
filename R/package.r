38fd5d10982dd6d751594bd8e55a42b42fd8a7df
Winston Chang <winston@stdout.org>
2012-08-10 13:38:05 -0500

    as.package: remove use of ~/.Rpackages, and better error message

--- R/package.r
+++ R/package.r
@@ -3,7 +3,6 @@
 #' Possible specifications of package:
 #' \itemize{
 #'   \item path
-#'   \item name (lookup in .Rpackages)
 #'   \item package object
 #' }
 #' @param x object to coerce to a package
@@ -20,9 +19,6 @@ as.package <- function(x = NULL) {
   }
   
   path <- find_package(x)
-  if (is.null(path)) {
-    stop("Can't find package ", x, call. = FALSE)
-  }
   
   pkg <- load_pkg_description(path)
   set_last_package(pkg)
@@ -44,37 +40,20 @@ local({
 })
 
 find_package <- function(x) {
-  is_package_path <- function(x) {
-    if (is.null(x)) return(FALSE)
-    
-    desc_path <- file.path(x, "DESCRIPTION")
-    file.exists(x) && file.exists(desc_path)
-  }
-  if (is_package_path(x)) {
+  if (is.null(x)) return(FALSE)
+
+  # Strip trailing slashes (needed only for windows)
+  x <- normalizePath(x, mustWork = FALSE)
+  x <- gsub("\\\\$", "", x)
+
+  desc_path <- file.path(x, "DESCRIPTION")
+
+  if ( file.exists(x) && file.exists(desc_path)) {
     return(x)
+  } else {
+    stop("Can't find package at '", file.path(normalizePath("."), x)
+      , "'", call. = FALSE)
   }
-
-  # Look for config file, and give up if it doesn't exist
-  config_path <- path.expand("~/.Rpackages")
-  if (!file.exists(config_path)) {
-    return(NULL)
-  }
-  lookup <- source(config_path)$value
-  
-  # Try special case
-  if (is_package_path(lookup[[x]])) {
-    return(lookup[[x]])
-  }
-
-  # Otherwise try default function, if it exists
-  if (!is.null(lookup$default)) {
-    default_loc <- lookup$default(x)
-    if (is_package_path(default_loc)) {
-      return(default_loc)
-    }
-  }
-
-  NULL
 }
 
 #' Load package DESCRIPTION into convenient form.
