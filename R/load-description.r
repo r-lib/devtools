7f23bffef09275d996dbc55f0ab60f2a65c58d96
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2012-08-10 14:13:52 -0400

    Remove unused and redundant load_description function.

--- R/load-description.r
+++
@@ -1,6 +0,0 @@
-#' Reads the DESCRIPTION file for the given package.
-#' @param path to the package
-#' @return description of the package
-#' @export
-load_description <- function(path)
-  read.dcf(file.path(path, 'DESCRIPTION'))
