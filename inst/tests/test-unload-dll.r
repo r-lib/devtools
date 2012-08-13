58946ecb1ca1a75bdb0fd303c1b8b8a0daab1622
Winston Chang <winston@stdout.org>
2012-08-13 13:20:54 -0500

    Add tests for loading DLLs

--- inst/tests/test-unload-dll.r
+++ inst/tests/test-unload-dll.r
@@ -1,6 +1,6 @@
 context("Compiled DLLs")
 
-test_that("unload() unloads DLLs", {
+test_that("unload() unloads DLLs from packages loaded with library()", {
 
   # Make a temp lib directory to install test package into
   old_libpaths <- .libPaths()
@@ -27,6 +27,46 @@ test_that("unload() unloads DLLs", {
 
   # Reset the libpath
   .libPaths(old_libpaths)
+
+  # Clean out compiled objects
+  clean_dll("unload-dll")
+})
+
+
+test_that("load_all() compiles and loads DLLs", {
+
+  clean_dll("unload-dll")
+
+  load_all("unload-dll", reset = TRUE)
+
+  # Check that it's loaded properly, by running a function from the package.
+  # nulltest() calls a C function which returns null.
+  expect_true(is.null(nulltest()))
+
+  # DLL should be listed in .dynLibs()
+  dynlibs <- vapply(.dynLibs(), `[[`, "name", FUN.VALUE = character(1))
+  expect_true(any(grepl("unloaddll", dynlibs)))
+
+  unload("unload-dll")
+
+  # DLL should not be listed in .dynLibs()
+  dynlibs <- vapply(.dynLibs(), `[[`, "name", FUN.VALUE = character(1))
+  expect_false(any(grepl("unloaddll", dynlibs)))
+
+
+  # Loading again, and reloading
+  # Should not re-compile (don't have a proper test for it)
+  load_all("unload-dll")
+  expect_true(is.null(nulltest()))
+
+  # Should not re-compile (don't have a proper test for it)
+  load_all("unload-dll")
+  expect_true(is.null(nulltest()))
+
+  # Should re-compile (don't have a proper test for it)
+  load_all("unload-dll", reset = TRUE)
+  expect_true(is.null(nulltest()))
+  unload("unload-dll")
 
   # Clean out compiled objects
   clean_dll("unload-dll")
