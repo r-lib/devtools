fc1e050a58098a969aa37b4dc983585b210361dc
Winston Chang <winston@stdout.org>
2012-08-13 21:45:45 -0500

    install_github works with pull requests

--- R/install-github.r
+++ R/install-github.r
@@ -6,6 +6,9 @@
 #' @param username Github username
 #' @param repo Repo name
 #' @param branch Desired branch - defaults to \code{"master"}
+#' @param pull Desired pull request. A pull request refers to a branch,
+#'   so you can't specify both \code{branch} and \code{pull}; one of
+#'   them must be \code{NULL}.
 #' @param subdir subdirectory within repo that contains the R package.
 #' @param ... Other arguments passed on to \code{\link{install}}.
 #' @export
@@ -14,7 +17,19 @@
 #' \dontrun{
 #' install_github("roxygen")
 #' }
-install_github <- function(repo, username = getOption("github.user"), branch = "master", subdir = NULL, ...) {
+install_github <- function(repo, username = getOption("github.user"),
+  branch = "master", pull = NULL, subdir = NULL, ...) {
+
+  if (!is.null(pull)) {
+    if (!is.null(branch)) {
+      stop("Can't specify both a branch and a pull request. ",
+       "Perhaps you want to use 'branch=NULL'?")
+    }
+    pullinfo <- github_pull_info(repo, username, pull)
+    username <- pullinfo$username
+    branch <- pullinfo$branch
+  }
+
   message("Installing github repo(s) ", 
     paste(repo, branch, sep = "/", collapse = ", "), 
     " from ", 
@@ -25,4 +40,17 @@ install_github <- function(repo, username = getOption("github.user"), branch = "
     "/zipball/", branch, sep = "")
 
   install_url(url, paste(repo, ".zip", sep = ""), subdir = subdir, ...)
+}
+
+# Retrieve the username and branch for a pull request
+github_pull_info <- function(repo, username, pull) {
+  host <- "https://api.github.com"
+  # GET /repos/:user/:repo/pulls/:number
+  path <- paste("repos", username, repo, "pulls", pull, sep = "/")
+  r <- GET(host, path = path)
+  stop_for_status(r)
+  head <- parsed_content(r)$head
+
+  list(repo = head$repo$name, username = head$repo$owner$login,
+    branch = head$ref)
 }
