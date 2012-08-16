1d988599c431f44f58f8e10071bc7dee99fa2a09
hadley <h.wickham@gmail.com>
2012-08-16 14:01:26 -0500

    Choose between html or txt docs

--- R/help.r
+++ R/help.r
@@ -7,6 +7,8 @@
 #' @param stage at which stage (‘"build"’, ‘"install"’, or ‘"render"’) should
 #'   \\Sexpr macros be executed? This is only important if you're using
 #'   \\Sexpr macro's in your Rd files.
+#' @param type of html to produce: \code{"html"} or \code{"text"}. Defaults to
+#'   your default documentation type.
 #' @examples
 #' \dontrun{
 #' library("ggplot2")
@@ -15,14 +17,15 @@
 #' load_all("ggplot2")
 #' dev_help("ggplot") # loads development documentation for ggplot
 #' }
-dev_help <- function(topic, stage = "render") {
+dev_help <- function(topic, stage = "render", type = getOption("help_type")) {
   path <- find_topic(topic)
   if (is.null(path)) {
     dev <- paste(dev_packages(), collapse = ", ")
     stop("Could not find topic ", topic, " in: ", dev)
   }
   
-  view_rd(path, stage = stage)  
+  pkg <- names(path)[1]
+  view_rd(path, pkg, stage = stage, type = type)
 } 
 
 #' Show an Rd file in a package.
@@ -46,12 +49,27 @@ show_rd <- function(pkg = NULL, file, ...) {
   }
 
   path <- file.path(pkg$path, "man", rd)  
-  view_rd(path, ...)
+  view_rd(path, pkg, ...)
 }
 
-view_rd <- function(path, package, stage = "render") {
-  temp <- Rd2txt(path, out = tempfile("Rtxt"), package = "In development", 
-    stages = stage)
-  file.show(temp, title = paste("Dev documentation: ", basename(path)), 
-    delete.file = TRUE) 
+view_rd <- function(path, package, stage = "render", type = getOption("help_type")) {
+  if (is.null(type)) type <- "text"
+  type <- match.arg(type, c("text", "html"))
+  
+  out_path <- paste(tempfile("Rtxt"), type, sep = ".")
+  
+  if (type == "text") {
+    Rd2txt(path, out = out_path, package = package, stages = stage)
+    file.show(out_path, title = paste(package, basename(path), sep = ":"))
+  } else if (type == "html") {
+    Rd2HTML(path, out = out_path, package = package, stages = stage, 
+      no_links = TRUE)
+      
+    css_path <- file.path(tempdir(), "R.css")
+    if (!file.exists(css_path)) {
+      file.copy(file.path(R.home("doc"), "html", "R.css"), css_path)
+    }
+      
+    browseURL(out_path)
+  }
 }
