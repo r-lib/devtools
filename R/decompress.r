fcfa04c1a362f9b8ccb0b129144f935fef850991
Winston Chang <winston@stdout.org>
2012-08-24 21:45:54 -0500

    decompress: code cleanup
    This commit doesn't change functionality, but it is a bit clearer.

--- R/decompress.r
+++ R/decompress.r
@@ -2,24 +2,24 @@ decompress <- function(src, target = tempdir()) {
   filename <- basename(src)
   
   if (grepl("\\.zip$", filename)) {
-    expand <- unzip
-    outdir <- function() {
-      getdir(as.character(expand(src, list = TRUE)$Name[1]))
-    }
+    unzip(src, exdir = target)
+    outdir <- getdir(as.character(unzip(src, list = TRUE)$Name[1]))
+
   } else if (grepl("\\.(tar\\.gz|tgz)$", filename)) {
-    expand <- function(...) untar(..., compressed = "gzip")
-    outdir <- function() getdir(expand(src, list = TRUE)[1])
+    untar(src, exdir = target, compressed = "gzip")
+    outdir <- getdir(untar(src, compressed = "gzip", list = TRUE)[1])
+
   } else if (grepl("\\.(tar\\.bz2|tbz)$", filename)) {
-    expand <- function(...) untar(..., compressed = "bzip2")
-    outdir <- function() getdir(expand(src, list = TRUE)[1])
+    untar(src, exdir = target, compressed = "bzip2")
+    outdir <- getdir(untar(src, compressed = "bzip2", list = TRUE)[1])
+
   } else {
     ext <- gsub("^[^.]*\\.", "", filename)
     stop("Don't know how to decompress files with extension ", ext, 
       call. = FALSE)
   }
-  expand(src, exdir = target)
   
-  file.path(target, outdir())
+  file.path(target, outdir)
 }
 
 
