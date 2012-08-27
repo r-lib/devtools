44b1ac66feb326e6ca06f487f745891a22842c27
hadley <h.wickham@gmail.com>
2012-08-27 09:17:35 -0500

    Nicer formatting for system commands

--- R/system.r
+++ R/system.r
@@ -1,7 +1,10 @@
 # @param arg a vector of command arguments.
 # @param env a named character vector.  Will be quoted
 system_check <- function(cmd, args = character(), env = character(), ...) {
-  message(cmd, " ", paste(args, collapse = ", "))
+  full <- paste(cmd, " ", paste(args, collapse = ", "), sep = "")
+  message(wrap_command(full))
+  
+  message()
   with_env(env, {
     res <- system2(cmd, args = args, ...)
   })
@@ -27,4 +30,10 @@ R <- function(options, path = tempdir(), ...) {
     # the R subprocesses. Unsetting it here avoids those problems.
 
   in_dir(path, system_check(r_path, options, env, ...))
+}
+
+wrap_command <- function(x) {
+  lines <- strwrap(x, getOption("width") - 2, exdent = 2)
+  continue <- c(rep(" \\", length(lines) - 1), "")
+  paste(lines, continue, collapse = "\n")
 }
