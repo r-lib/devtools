78d12fabe4895e711d3957745909f3b9abff8142
Winston Chang <winston@stdout.org>
2012-09-12 12:46:51 -0500

    Update extra file tests to use check_devtools

--- inst/tests/test-build.r
+++
@@ -1,9 +0,0 @@
-context("Building")
-
-test_that("Message about extra files in package", {
-
-  expect_message(buildfile <- build("build-extrafile"),
-    regexp = "found.*an_extra_file")
-
-  unlink(buildfile)
-})
