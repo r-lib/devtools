5ad43b0a5a112fdab09a11ddcdd88f2016c8a884
hadley <h.wickham@gmail.com>
2012-09-13 15:25:22 -0500

    Build vignettes in build(). Fixes #155

--- R/build.r
+++ R/build.r
@@ -1,5 +1,5 @@
 #' Build package in specified directory.
-#' 
+#'
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
 #' @param path path in which to produce package.  If \code{NULL}, defaults to
@@ -15,13 +15,13 @@ build <- function(pkg = ".", path = NULL, binary = FALSE) {
   if (is.null(path)) {
     path <- dirname(pkg$path)
   }
-  
+
   if (binary) {
     cmd <- paste("CMD INSTALL ", shQuote(pkg$path), " --build", sep = "")
     ext <- if (.Platform$OS.type == "windows") "zip" else "tgz"
   } else {
-    cmd <- paste("CMD build ", shQuote(pkg$path), 
-      " --no-manual --no-vignettes --no-resave-data", sep = "")
+    cmd <- paste("CMD build ", shQuote(pkg$path),
+      " --no-manual --no-resave-data", sep = "")
     ext <- "tar.gz"
   }
   R(cmd, path)
@@ -36,26 +36,26 @@ build <- function(pkg = ".", path = NULL, binary = FALSE) {
 #'
 #' Works by building source package, and then uploading to
 #' \url{http://win-builder.r-project.org/}.  Once building is complete you'll
-#' receive a link to the built package in the email address listed in the 
+#' receive a link to the built package in the email address listed in the
 #' maintainer field.  It usually takes around 30 minutes.
 #'
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
 #' @param version directory to upload to on the win-builder, controlling
 #'   which version of R is used to build the package. Possible options are
-#'   listed on \url{http://win-builder.r-project.org/}. Defaults to the 
+#'   listed on \url{http://win-builder.r-project.org/}. Defaults to the
 #'   released version of R.
 #' @importFrom RCurl ftpUpload
 #' @export
 #' @family build functions
 build_win <- function(pkg = ".", version = "R-release") {
   pkg <- as.package(pkg)
-  message("Building windows version of ", pkg$package, 
+  message("Building windows version of ", pkg$package,
     " with win-builder.r-project.org.\nCheck your email for link to package.")
 
   built_path <- build(pkg, tempdir())
   on.exit(unlink(built_path))
-  
+
   ftpUpload(built_path, paste("ftp://win-builder.r-project.org/", version,
     "/", basename(built_path), sep = ""))
   invisible()
