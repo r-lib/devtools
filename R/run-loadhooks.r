5d97fd02adcc0f4465686fe79d72213683438543
Winston Chang <winston@stdout.org>
2012-12-15 14:45:42 -0600

    run_onload: call .onLoad with package name

--- R/run-loadhooks.r
+++ R/run-loadhooks.r
@@ -6,6 +6,13 @@
 #'
 #' A variable called \code{onLoad} is created in the package's
 #' devtools metdata to indicate that it's attached.
+#'
+#' When a package is loaded with \code{library()} it calls
+#' \code{.onLoad(libpath, pkg)}, where \code{libpath} is the library path for
+#' package, and \code{pkg} is the name of the package. Because in-development
+#' packages are not installed to a library, this function can't call
+#' \code{.onLoad} with a proper libpath, so it uses \code{NULL} instead.
+#'
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
 run_onload <- function(pkg = ".") {
@@ -16,7 +23,7 @@ run_onload <- function(pkg = ".") {
   # Run .onLoad if it's defined. Set a flag 'onLoad' in the metadata
   if (exists(".onLoad", nsenv, inherits = FALSE) &&
      is.null(metadata$onLoad)) {
-    nsenv$.onLoad()
+    nsenv$.onLoad(NULL, pkg$package)
     metadata$onLoad <- TRUE
   }
 }
