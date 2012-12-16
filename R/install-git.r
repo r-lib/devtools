473d865a6867ec83f39e9056911b1ea159fb5a21
Winston Chang <winston@stdout.org>
2012-12-15 18:25:48 -0600

    Code cleanup to better conform to style

--- R/install-git.r
+++ R/install-git.r
@@ -1,32 +1,31 @@
 #' Install a package from a git repository
 #'
-#' It is important to note that this function requires "Git" to be
-#' installed on your system in order to be used.
+#' This function requires \code{git} to be installed on your system in order to
+#' be used.
 #'
-#' This function is vectorised so you can install multiple packages in 
+#' It is vectorised so you can install multiple packages with
 #' a single command.
 #'
-#' @param git_url Location of package on internet. The url should point to a
-#'                public or private repository.
+#' @param git_url Location of package. The url should point to a public or
+#'                private repository.
 #' @param name    Optional package name, used to provide more informative
-#'                messages
+#'                messages.
 #' @param subdir  A sub-directory withing a git repository that may
 #'                contain the package we are interested in installing.
-#'                
 #' @param git_binary A custom git-binary to use instead of default system's git
 #'                   version.
 #' @param config     Additional configuration argument.
 #' @param ...        Other arguments passed on to \code{\link{install}}.
 #' @export
 #' @family package installation
-install_git <- function(git_url, name = NULL, subdir = NULL, 
-                        git_binary = NULL, config = list(), ...) {
+install_git <- function(git_url, name = NULL, subdir = NULL,
+  git_binary = NULL, config = list(), ...) {
 
   if (is.null(name)) {
     name <- rep(list(NULL), length(git_url))
   }
-  
-  invisible(mapply(install_git_single, git_url, name, 
+
+  invisible(mapply(install_git_single, git_url, name,
     MoreArgs = list(
       subdir = subdir, git_binary = git_binary, config = config, ...
     )
@@ -34,23 +33,31 @@ install_git <- function(git_url, name = NULL, subdir = NULL,
 }
 
 #' Install a single package from a git repository
-#' 
+#'
 #' This function allows you to install a single package from a git repository.
-#' 
+#'
 #' See \code{\link{install_git}} for more information about the paraemeters.
-install_git_single <- function(git_url, name = NULL, subdir = NULL, 
-                               git_binary = NULL, config = list(), ...) {
+#'
+#' @param git_url Location of package. The url should point to a public or
+#'                private repository.
+#' @param name    Optional package name, used to provide more informative
+#'                messages.
+#' @param subdir  A sub-directory withing a git repository that may
+#'                contain the package we are interested in installing.
+#' @param git_binary A custom git-binary to use instead of default system's git
+#'                   version.
+install_git_single <- function(git_url, name = NULL, subdir = NULL,
+  git_binary = NULL, config = list(), ...) {
+
   if (is.null(name)) {
-    name <- basename(git_url) 
+    name <- gsub("\\.git$", "", basename(git_url))
   }
 
-  name <- gsub("\\.git$", "", name)
-
   message("Preparing installation of ", name, " using the Git-URL: ", git_url)
-  
+
   # Unique temporary file-name.
   bundle <- tempfile()
- 
+
   # \code{git_path} will handle the NULL and return the system default.
   git_binary_path <- git_path(git_binary)
 
@@ -58,19 +65,19 @@ install_git_single <- function(git_url, name = NULL, subdir = NULL,
   # @TODO: Handle configs, this currently only supports public repos
   #        and repositories with the public SSH key set.
   request <- system2(
-    git_binary_path, 
-    args = c('clone', '--depth', '1', '--no-hardlinks', git_url,  bundle), 
+    git_binary_path,
+    args = c('clone', '--depth', '1', '--no-hardlinks', git_url,  bundle),
     stdout = FALSE, stderr = FALSE
   )
- 
+
   # This is only looking for an error code above 0-success
   if (request > 0) {
     stop("There seems to be a problem retrieving this Git-URL.", call. = FALSE)
   }
-  
+
   pkg_path <- if (is.null(subdir)) bundle else file.path(bundle, subdir)
   on.exit(unlink(bundle), add = TRUE)
-  
+
   # Check it's an R package
   if (!file.exists(file.path(pkg_path, "DESCRIPTION"))) {
     stop("Does not appear to be an R package", call. = FALSE)
@@ -80,7 +87,7 @@ install_git_single <- function(git_url, name = NULL, subdir = NULL,
   if (file.exists(config_path)) {
     Sys.chmod(config_path, "755")
   }
-  
+
   # Install
   install(pkg_path, ...)
 }
@@ -90,27 +97,25 @@ install_git_single <- function(git_url, name = NULL, subdir = NULL,
 #' @param git_binary_name The name of the binary depending on the OS.
 git_path <- function(git_binary_name = NULL) {
   os_type     <- .Platform$OS.type
-  binary_name <- NULL
-  
+
   if (!is.null(git_binary_name)) {
     if (!file.exists(git_binary_name)) {
       stop(
-        "The Git binary you specified ", 
+        "The Git binary you specified ",
         "does not appear to be installed on ",
         "your system.", call. = FALSE
       )
     }
     git_path <- git_binary_name
-  } else {  
+  } else {
     # Decide on system default.
-    binary_name <- if (os_type == "unix") "git" else "git.exe"
+    binary_name <- if (.Platform$OS.type == "windows") "git.exe" else "git"
     git_path    <- unname(Sys.which(binary_name))
-    
+
     if (git_path == "") {
       stop("Git does not seem to be installed on your system.", call. = FALSE)
     }
   }
-  
-  # Only using explicit return for clarity.
-  return(git_path)
+
+  git_path
 }
