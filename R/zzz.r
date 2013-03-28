801d6b1d3a889e2523211bbf369290748d0cc761
hadley <h.wickham@gmail.com>
2013-03-28 10:42:32 -0500

    Tweak style

--- R/zzz.r
+++ R/zzz.r
@@ -6,9 +6,9 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
   op.devtools <- list(
-    devtools.path="~/R-dev",
-    devtools.cleandoc=FALSE,
-    github.user="hadley"
+    devtools.path = "~/R-dev",
+    devtools.cleandoc = FALSE,
+    github.user = "hadley"
   )
   toset <- !(names(op.devtools) %in% names(op))
   if(any(toset)) options(op.devtools[toset])
