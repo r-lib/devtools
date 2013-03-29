1268b162bc06bc6f6284a0ab5e7329c023ce14ce
hadley <h.wickham@gmail.com>
2013-03-29 06:29:48 -0500

    More options for with_envvar

--- R/with.r
+++ R/with.r
@@ -27,6 +27,10 @@
 #' Sys.getenv("HADLEY")
 #' with_envvar(c("HADLEY" = 2), Sys.getenv("HADLEY"))
 #' Sys.getenv("HADLEY")
+#'
+#' with_envvar(c("A" = 1),
+#'   with_envvar(c("A" = 2), action = "suffix", Sys.getenv("A"))
+#' )
 NULL
 
 with_something <- function(set) {
@@ -42,20 +46,41 @@ is.named <- function(x) {
 
 # env ------------------------------------------------------------------------
 
-set_envvar <- function(envs) {
+set_envvar <- function(envs, action = "replace") {
   stopifnot(is.named(envs))
+  stopifnot(is.character(action), length(action) == 1)
+  action <- match.arg(action, c("replace", "prefix", "suffix"))
 
   old <- Sys.getenv(names(envs), names = TRUE, unset = NA)
-
   set <- !is.na(envs)
-  if (any(set)) do.call("Sys.setenv", as.list(envs[set]))
+
+  both_set <- set & !is.na(old)
+  if (any(both_set)) {
+    if (action == "prefix") {
+      envs[both_set] <- paste(envs[both_set], old[both_set])
+    } else if (action == "suffix") {
+      envs[both_set] <- paste(old[both_set], envs[both_set])
+    }
+  }
+
+  if (any(set))  do.call("Sys.setenv", as.list(envs[set]))
   if (any(!set)) Sys.unsetenv(names(envs)[!set])
 
   invisible(old)
 }
 #' @rdname with_something
+#' @param action (for \code{with_envvar} only): should new values
+#'    \code{"replace"}, \code{"suffix"}, \code{"prefix"} existing environmental
+#'    variables with the same name.
 #' @export
-with_envvar <- with_something(set_envvar)
+with_envvar <- function(new, code, action = "replace") {
+  old <- set_envvar(new, action)
+  on.exit(set_envvar(old, "replace"))
+  force(code)
+}
+
+
+with_something(set_envvar)
 
 #' @rdname with_something
 #' @export
--- inst/tests/test-with.r
+++ inst/tests/test-with.r
@@ -2,15 +2,32 @@
 context("With")
 
 test_that("with_envvar sets and unsets variables", {
-  
+
   # Make sure the "set_env_testvar" environment var is not set.
   Sys.unsetenv("set_env_testvar")
   expect_false("set_env_testvar" %in% names(Sys.getenv()))
-  
+
   # Use with_envvar (which calls set_envvar) to temporarily set it to 1
-  expect_identical("1", with_envvar(c("set_env_testvar" = 1), 
+  expect_identical("1", with_envvar(c("set_env_testvar" = 1),
     Sys.getenv("set_env_testvar")))
-  
-  # set_env_testvar shouldn't stay in the list of environment vars 
+
+  # set_env_testvar shouldn't stay in the list of environment vars
   expect_false("set_env_testvar" %in% names(Sys.getenv()))
+})
+
+test_that("with_envar respects suffix and prefix", {
+  nested <- function(op1, op2) {
+    with_envvar(c(A = 1), action = op1,
+      with_envvar(c(A = 2), action = op2,
+        Sys.getenv("A")[[1]]
+      )
+    )
+  }
+
+  expect_equal(nested("replace", "suffix"), c("1 2"))
+  expect_equal(nested("replace", "prefix"), c("2 1"))
+  expect_equal(nested("prefix", "suffix"), c("1 2"))
+  expect_equal(nested("prefix", "prefix"), c("2 1"))
+  expect_equal(nested("suffix", "suffix"), c("1 2"))
+  expect_equal(nested("suffix", "prefix"), c("2 1"))
 })
