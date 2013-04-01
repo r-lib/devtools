a936dbce52583abda30de11490d0ac424533cef6
hadley <h.wickham@gmail.com>
2013-04-01 15:37:31 -0500

    Move source functions into own file

---
+++ R/source.r
@@ -0,0 +1,33 @@
+source_many <- function(files, envir = parent.frame()) {
+  stopifnot(is.character(files))
+  stopifnot(is.environment(envir))
+
+  oop <- options(
+    keep.source = TRUE,
+    show.error.locations = TRUE,
+    topLevelEnvironment = as.environment(envir))
+  on.exit(options(oop))
+
+  for (file in files) {
+    source_one(file, envir = envir)
+  }
+  invisible()
+}
+
+source_one <- function(file, envir = parent.frame()) {
+  stopifnot(file.exists(file))
+  stopifnot(is.environment(envir))
+
+  lines <- readLines(file, warn = FALSE)
+  srcfile <- srcfilecopy(file, lines, file.info(file)[1, "mtime"],
+    isFile = TRUE)
+  exprs <- parse(text = lines, n = -1)
+
+  n <- length(exprs)
+  if (n == 0L) return(invisible())
+
+  for (i in seq_len(n)) {
+    eval(exprs[i], envir)
+  }
+  invisible()
+}
