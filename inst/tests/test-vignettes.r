2b39e46ae2b6f82508ed4f19329ad7d5d19b0f36
hadley <h.wickham@gmail.com>
2013-04-11 13:23:54 -0500

    Only test markdown vignettes with latest knitr

--- inst/tests/test-vignettes.r
+++ inst/tests/test-vignettes.r
@@ -17,25 +17,28 @@ test_that("Sweave vignettes copied into inst/doc", {
   expect_false("new.Rnw" %in% dir("testVignettes/inst/doc"))
 })
 
-test_that("Rmarkdown vignettes copied into inst/doc", {
-  pkg <- as.package("testMarkdownVignettes")
-  doc_path <- file.path(pkg$path, "inst", "doc")
+if (packageVersion("knitr") >= 1.2) {
+  test_that("Rmarkdown vignettes copied into inst/doc", {
+    pkg <- as.package("testMarkdownVignettes")
+    doc_path <- file.path(pkg$path, "inst", "doc")
 
-  clean_vignettes(pkg)
-  expect_false("test.html" %in% dir(doc_path))
-  expect_false("test.R" %in% dir(doc_path))
-  expect_false("test.Rmd" %in% dir(doc_path))
+    clean_vignettes(pkg)
+    expect_false("test.html" %in% dir(doc_path))
+    expect_false("test.R" %in% dir(doc_path))
+    expect_false("test.Rmd" %in% dir(doc_path))
 
-  build_vignettes(pkg)
-  expect_true("test.html" %in% dir(doc_path))
-  expect_true("test.R" %in% dir(doc_path))
-  expect_true("test.Rmd" %in% dir(doc_path))
+    build_vignettes(pkg)
+    expect_true("test.html" %in% dir(doc_path))
+    expect_true("test.R" %in% dir(doc_path))
+    expect_true("test.Rmd" %in% dir(doc_path))
 
-  clean_vignettes(pkg)
-  expect_false("test.html" %in% dir(doc_path))
-  expect_false("test.R" %in% dir(doc_path))
-  expect_false("test.Rmd" %in% dir(doc_path))
-})
+    clean_vignettes(pkg)
+    expect_false("test.html" %in% dir(doc_path))
+    expect_false("test.R" %in% dir(doc_path))
+    expect_false("test.Rmd" %in% dir(doc_path))
+  })
+}
+
 
 test_that("Extra files copied and removed", {
   pkg <- as.package("testVignetteExtras")
