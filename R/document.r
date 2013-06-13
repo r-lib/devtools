d6f1283fdf96a62b1b0bca34e09431f7a51e3502
hadley <h.wickham@gmail.com>
2013-06-13 08:47:38 -0500

    Tweaks to check_doc

--- R/document.r
+++ R/document.r
@@ -60,18 +60,36 @@ document <- function(pkg = ".", clean = FALSE,
 #'
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
+#' @return Nothing. This function is called purely for it's side effects: if
+#   no errors there will be no output.
 #' @export
+#' @importFrom tools checkDocFiles
+#' @examples
+#' \dontrun{
+#' document("mypkg")
+#' check_doc("mypkg")
+#' }
 check_doc <- function(pkg = ".") {
   pkg <- as.package(pkg)
   old <- options(warn = -1)
   on.exit(options(old))
 
-  print(tools:::.check_package_parseRd(dir = pkg$path))
-  print(tools:::.check_Rd_metadata(dir = pkg$path))
-  print(tools:::.check_Rd_xrefs(dir = pkg$path))
-  print(tools:::.check_Rd_contents(dir = pkg$path))
+  print_if_not_null(tools:::.check_package_parseRd(dir = pkg$path))
+  print_if_not_null(tools:::.check_Rd_metadata(dir = pkg$path))
+  print_if_not_null(tools:::.check_Rd_xrefs(dir = pkg$path))
+  print_if_not_null(tools:::.check_Rd_contents(dir = pkg$path))
 
-  print(tools::checkDocFiles(dir = pkg$path))
-  # print(tools::checkDocStyle(dir = pkg$path))
-  # print(tools::undoc(dir = pkg$path))
+  print_if_not_null(checkDocFiles(dir = pkg$path))
+  # Can't run because conflicts with how devtools loads code
+  # print_if_not_null(checkDocStyle(dir = pkg$path))
+  # print_if_not_null(checkReplaceFuns(dir = pkg$path))
+  # print_if_not_null(checkS3methods(dir = pkg$path))
+  # print(undoc(dir = pkg$path))
+
+  invisible()
+}
+
+print_if_not_null <- function(x) {
+  if (is.null(x)) return(invisible())
+  print(x)
 }
