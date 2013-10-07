174a5c315ac1db5ee804fd0d76c654a1bad8da6f
hadley <h.wickham@gmail.com>
2013-10-07 12:52:10 -0500

    Relax description requirements. Closes #351

--- tests/testthat/test-description.r
+++ tests/testthat/test-description.r
@@ -1,13 +1,5 @@
 context("DESCRIPTION checks")
 
-test_that("invalid DESCRIPTION gives warning", {
-  expect_message(load_all("testInvalidDescription"), "Invalid DESCRIPTION")
-  expect_message(load_all("testInvalidDescription"), "fields missing")
-
-  unload("testInvalidDescription")
-})
-
-
 test_that("Parse DESCRIPTION file", {
   pkg <- as.package("testNamespace")
 
