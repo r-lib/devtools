7954ba3bba4cda8e15ec932abc36f6ee24560c17
hadley <h.wickham@gmail.com>
2013-11-14 13:18:03 -0600

    Add roxygen3 support

--- R/document.r
+++ R/document.r
@@ -30,14 +30,25 @@ document <- function(pkg = ".", clean = FALSE,
   if (reload) {
     load_all(pkg, reset = clean)
   }
+  
+  if (packageVersion("roxygen2") < 3) {
+    document_roxygen2(pkg, roclets)
+  } else {
+    document_roxygen3(pkg, roclets)
+  }
+  
+  clear_topic_index(pkg)
+  invisible()
+}
 
+document_roxygen2 <- function(pkg, roclets, reload = TRUE) {  
   # Integrate source and evaluated code
   env <- ns_env(pkg)
   env_hash <- suppressWarnings(digest(env))
   r_files <- find_code(pkg)
   parsed <- unlist(lapply(r_files, parse.file, env = env,
     env_hash = env_hash), recursive = FALSE)
-
+  
   roclets <- paste(roclets, "_roclet", sep = "")
   for (roclet in roclets) {
     roc <- match.fun(roclet)()
@@ -48,8 +59,10 @@ document <- function(pkg = ".", clean = FALSE,
       })
     )
   }
-
-  clear_topic_index(pkg)
-  invisible()
 }
 
+document_roxygen3 <- function(pkg, roclets, reload = TRUE) {
+  with_envvar(r_env_vars(), with_collate("C", 
+    roxygenise(pkg$path, roclets = roclets, load_code = pkg_env)
+  ))
+}
