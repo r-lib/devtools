093299e423f8ad1f5294c9b1438fb7f1b8e4f18a
hadley <h.wickham@gmail.com>
2013-11-18 09:14:58 -0600

    Don't try and clear caches for roxygen2 3.0

--- R/document.r
+++ R/document.r
@@ -23,7 +23,9 @@ document <- function(pkg = ".", clean = FALSE,
   if (!file.exists(man_path)) dir.create(man_path)
 
   if (clean) {
-    roxygen2::clear_caches()
+    if (packageVersion("roxygen2") < 3) {
+      roxygen2::clear_caches()  
+    }
     file.remove(dir(man_path, full.names = TRUE))
   }
 
