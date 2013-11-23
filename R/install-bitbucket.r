efe9be6b84397bf07bd8277157227e0bfecb622f
Imanuel Costigan <i.costigan@me.com>
2013-11-23 22:44:21 +1100

    Enable pull argument for bitbucket

--- R/install-bitbucket.r
+++ R/install-bitbucket.r
@@ -15,12 +15,24 @@
 #' install_bitbucket("paulhiemstra")
 #' install_bitbucket(c("testrepo", "testrepo2"))
 #' }
-install_bitbucket <- function(repo, username, ref = "master", branch = NULL,
+install_bitbucket <- function(repo, username=getOption('bitbucket.user'), 
+                              ref = "master", pull = NULL, branch = NULL,
                               auth_user = NULL, password = NULL, ...)
 {
   if (!is.null(branch)) {
     warning("'branch' is deprecated. In the future, please use 'ref' instead.")
     ref <- branch
+  }
+  
+  if (!xor(is.null(pull), is.null(ref))) {
+    stop("Must specify either a ref or a pull request, not both. ",
+         "Perhaps you want to use 'ref=NULL'?")
+  }
+  
+  if(!is.null(pull)) {
+    pullinfo <- bitbucket_pull_info(repo, username, pull)
+    username <- pullinfo$username
+    ref <- pullinfo$ref
   }
 
   if (!is.null(password)) {
@@ -68,4 +80,22 @@ install_bitbucket <- function(repo, username, ref = "master", branch = NULL,
   }
   install_url(url, name = paste(repo, ".zip", sep=""),
               config = auth, before_install = bitbucket_before_install, ...)
-  }
+}
+
+bitbucket_pull_info <- function (repo, username, pull) {
+  host <- "https://bitbucket.org/api/2.0"
+  # https://bitbucket.org/api/2.0/repositories/{owner}/{repo_slug}/pullrequests/{id}
+  # Unofficial, but official: 
+  # http://restbrowser.bitbucket.org
+  path <- paste("repositories", username, repo, "pullrequests", pull, sep = "/")
+  r <- GET(paste(host, path, sep='/'))
+  stop_for_status(r)
+  head <- parsed_content(r)
+  
+  repo <- head$source$repository$full_name
+  repo <- unlist(strsplit(repo, '/'))
+  
+  list(repo = repo[2], 
+       username = repo[1],
+       ref = head$source$branch$name)
+}
