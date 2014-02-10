842cb510bbcd984b4062b5d792108de61363037a
hadley <h.wickham@gmail.com>
2014-02-10 08:49:28 -0600

    More advice in add_travis()

--- R/travis.R
+++ R/travis.R
@@ -12,7 +12,10 @@ add_travis <- function(pkg = ".") {
   if (file.exists(path)) {
     stop(".travis.yml already exists", call. = FALSE)
   }
-  message("Adding .travis.yml to ", pkg$package)
+  message("Adding .travis.yml to ", pkg$package, "Next: \n",
+    " * Turn on travis for this repo at https://travis-ci.org/profile\n",
+    " * Add a travis shield to your README.md"
+  )
 
   template_path <- system.file("templates/.travis.yml", package = "devtools")
   file.copy(template_path, path)
