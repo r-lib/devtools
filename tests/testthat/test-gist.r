bde4fb81e7396fafd655a014a0b095f082c8e78b
Wush Wu <wush978@gmail.com>
2014-07-12 05:58:35 +0800

    Fix the bug of calling R CMD SHLIB during testing.
    The `sourceCpp` will call `R CMD SHLIB` which is affected by the environmental variable "R_TESTS".

--- tests/testthat/test-gist.r
+++ tests/testthat/test-gist.r
@@ -1,5 +1,10 @@
 context("Gist")
 
+# The tests need to call R CMD ...
+# Therefore I need to change the environment variable to make it normal
+.R_TESTS <- Sys.getenv("R_TESTS")
+Sys.setenv("R_TESTS"="")
+
 test_that("Source Gist Correctly", {
   expect_equal(capture.output(source_gist("108b497a9cbbcd3caa9a"), file=NULL), 
                "asdkfja;sldfa;sl")
@@ -16,5 +21,5 @@ test_that("Source Gist Correctly", {
   }, "hello")
   expect_error(source_gist("f3fe88494ad49fe9445e", sha1="294673"))
 })
-
+Sys.setenv("R_TESTS"=.R_TESTS)
 
