4bd2ccd881786cfa9ca83e5f4c19d2435c805061
hadley <h.wickham@gmail.com>
2014-08-14 10:52:24 -0500

    Add remote class & objects for github

--- R/install-github.r
+++ R/install-github.r
@@ -19,8 +19,6 @@
 #'   you can easily delete a PAT without affecting any others. Defaults to
 #'   the \code{GITHUB_PAT} environment variable.
 #' @param ... Other arguments passed on to \code{\link{install}}.
-#' @param dependencies By default, installs all dependencies so that you can
-#'   build vignettes and use all functionality of the package.
 #' @export
 #' @family package installation
 #' @seealso \code{\link{github_pull}}
@@ -45,36 +43,52 @@
 #' }
 install_github <- function(repo, username = NULL,
                            ref = "master", subdir = NULL,
-                           auth_token = github_pat(), ...,
-                           dependencies = TRUE) {
+                           auth_token = github_pat(), ...) {
 
-  pkgs <- lapply(repo, github_pkg, username = username, ref = ref,
-    subdir = subdir, auth_token = auth_token, ..., dependencies = dependencies)
+  remotes <- lapply(repo, github_remote, username = username, ref = ref,
+    subdir = subdir, auth_token = auth_token)
 
-  lapply(pkgs, install_remote)
-
+  install_remotes(remotes, ...)
 }
 
-github_pkg <- function(repo, username = NULL, ref = NULL, subdir = NULL,
-                       auth_token = github_pat(), ...) {
+github_remote <- function(repo, username = NULL, ref = NULL, subdir = NULL,
+                       auth_token = github_pat(), sha = NULL) {
 
   meta <- parse_git_repo(repo)
 
   if (is.null(meta$username)) {
-    meta$username <- username %||% getOption("github.user")
-    warning("Relying on default username is deprecated. Please use ",
+    meta$username <- username %||% getOption("github.user") %||%
+      stop("Unknown username.")
+    warning("Username parameter is deprecated. Please use ",
       username, "/", repo, call. = FALSE)
-  } else {
-    username <- meta$username
   }
-  meta$subdir <- meta$subdir %||% subdir
+
   if (is.null(meta$ref) && !is.null(ref)) {
     meta <- resolve_ref(ref, meta)
   }
 
-  if (!is.null(auth_token)) {
+  remote("github",
+    repo = meta$repo,
+    subdir = meta$subdir %||% subdir,
+    username = meta$username,
+    ref = meta$ref,
+    sha = sha,
+    auth_token = auth_token
+  )
+}
+
+remote_download.github_remote <- function(x, quiet = FALSE) {
+  if (!quiet) {
+    message("Downloading github repo ", x$username, "/", x$repo, "@", x$ref)
+  }
+
+  dest <- tempfile(fileext = paste0(".zip"))
+  src <- file.path("https://api.github.com", "repos", x$username, x$repo,
+    "zipball", x$ref)
+
+  if (!is.null(x$auth_token)) {
     auth <- httr::authenticate(
-      user = auth_token,
+      user = x$auth_token,
       password = "x-oauth-basic",
       type = "basic"
     )
@@ -82,16 +96,36 @@ github_pkg <- function(repo, username = NULL, ref = NULL, subdir = NULL,
     auth <- NULL
   }
 
-  meta$SHA1 <- github_commit(meta$username, meta$repo, meta$ref)$sha
+  download(dest, src, auth)
+}
 
-  url <- file.path("https://api.github.com", "repos", meta$username,
-    meta$repo, "zipball", meta$ref)
+remote_metadata.github_remote <- function(x, bundle = NULL, source = NULL) {
+  # Determine sha as efficiently as possible
+  if (!is.null(x$sha)) {
+    # Might be cached already (because re-installing)
+    sha <- x$sha
+  } else if (!is.null(bundle)) {
+    # Might be able to get from zip archive
+    sha <- git_extract_sha1(bundle)
+  } else {
+    # Otherwise can use github api
+    sha <- github_commit(x$username, x$repo, x$ref)$sha
+  }
 
-  msg <- paste0("Downloading github repo ", meta$username, "/", meta$repo,
-    "@", meta$ref)
-
-  list(url = url, name = meta$repo, config = auth, meta = meta, message = msg,
-    type = "zip")
+  list(
+    RemoteType = "github",
+    RemoteRepo = x$repo,
+    RemoteUsername = x$username,
+    RemoteRef = x$ref,
+    RemoteSha = sha,
+    RemoteSubdir = x$subdir,
+    # Backward compatibility for packrat etc.
+    GithubRepo = x$repo,
+    GithubUsername = x$username,
+    GithubRef = x$ref,
+    GithubSHA1 = sha,
+    GithubSubdir = x$subdir
+  )
 }
 
 #' Install a specific pull request from GitHub
@@ -123,7 +157,7 @@ resolve_ref.github_pull <- function(x, params) {
 
 # Parse concise git repo specification: username/repo[/subdir][#pull|@ref]
 parse_git_repo <- function(path) {
-  username_rx <- "(?:([^/]+)/)"
+  username_rx <- "(?:([^/]+)/)?"
   repo_rx <- "([^/@#]+)"
   subdir_rx <- "(?:/([^@#]*[^@#/]))?"
   ref_rx <- "(?:@(.+))"
--- R/install-remote.R
+++ R/install-remote.R
@@ -9,21 +9,21 @@
 #' }
 #' @noRd
 install_remote <- function(remote, ..., quiet = FALSE) {
-  bundle <- tempfile(fileext = paste0(".", remote$type))
-  if (!quiet) {
-    message(pkg$message)
-  }
-  download(bundle, remote$url, remote$config)
+  stopifnot(is.remote(remote))
+
+  bundle <- remote_download(remote, quiet = quiet)
   on.exit(unlink(bundle), add = TRUE)
 
-  pkg_path <- source_pkg(bundle, subdir = remote$meta$subdir)
-  on.exit(unlink(pkg_path, recursive = TRUE), add = TRUE)
+  source <- source_pkg(bundle, subdir = remote$subdir)
+  on.exit(unlink(source, recursive = TRUE), add = TRUE)
 
-  meta <- remote$meta
-  names(meta) <- paste0("Github", first_upper(names(meta)))
-  add_metadata(pkg_path, meta)
+  add_metadata(source, remote_metadata(remote, bundle, source))
 
-  install(pkg_path, ..., quiet = quiet)
+  install(source, ..., quiet = quiet)
+}
+
+install_remotes <- function(remotes, ...) {
+  invisible(vapply(remotes, install_remote, ..., FUN.VALUE = logical(1)))
 }
 
 # Add metadata
@@ -35,3 +35,11 @@ add_metadata <- function(pkg_path, meta) {
 
   write_dcf(path, desc)
 }
+
+remote <- function(type, ...) {
+  structure(list(...), class = c(paste0(type, "_remote"), "remote"))
+}
+is.remote <- function(x) inherits(x, "remote")
+
+remote_download <- function(x, quiet = FALSE) UseMethod("remote_download")
+remote_metadata <- function(x, bundle = NULL, source = NULL) UseMethod("remote_metadata")
