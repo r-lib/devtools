38bfbdce65049ff3d51c13419c05d48c22b6ab6e
hadley <h.wickham@gmail.com>
2014-08-14 10:52:24 -0500

    Add remote class & objects for github

--- R/git.R
+++ R/git.R
@@ -77,3 +77,27 @@ git_path <- function(git_binary_name = NULL) {
 
   stop("Git does not seem to be installed on your system.", call. = FALSE)
 }
+
+# Extract the commit hash from a git archive. Git archives include the SHA1
+# hash as the comment field of the zip central directory record
+# (see https://www.kernel.org/pub/software/scm/git/docs/git-archive.html)
+# Since we know it's 40 characters long we seek that many bytes minus 2
+# (to confirm the comment is exactly 40 bytes long)
+git_extract_sha1 <- function(bundle) {
+
+  # open the bundle for reading
+  conn <- file(bundle, open = "rb", raw = TRUE)
+  on.exit(close(conn))
+
+  # seek to where the comment length field should be recorded
+  seek(conn, where = -0x2a, origin = "end")
+
+  # verify the comment is length 0x28
+  len <- readBin(conn, "raw", n = 2)
+  if (len[1] == 0x28 && len[2] == 0x00) {
+    # read and return the SHA1
+    rawToChar(readBin(conn, "raw", n = 0x28))
+  } else {
+    NULL
+  }
+}
