994d5a829c70d2399094705ef44305378709358d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-08-22 14:50:49 +0200

    final amendments

--- R/install-github.r
+++ R/install-github.r
@@ -160,31 +160,6 @@ github_resolve_ref.NULL <- function(x, params) {
   params
 }
 
-# Extract the commit hash from a github bundle and append it to the
-# package DESCRIPTION file. Git archives include the SHA1 hash as the
-# comment field of the zip central directory record
-# (see https://www.kernel.org/pub/software/scm/git/docs/git-archive.html)
-# Since we know it's 40 characters long we seek that many bytes minus 2
-# (to confirm the comment is exactly 40 bytes long)
-github_extract_sha1 <- function(bundle) {
-
-  # open the bundle for reading
-  conn <- file(bundle, open = "rb", raw = TRUE)
-  on.exit(close(conn))
-
-  # seek to where the comment length field should be recorded
-  seek(conn, where = -0x2a, origin = "end")
-
-  # verify the comment is length 0x28
-  len <- readBin(conn, "raw", n = 2)
-  if (len[1] == 0x28 && len[2] == 0x00) {
-    # read and return the SHA1
-    rawToChar(readBin(conn, "raw", n = 0x28))
-  } else {
-    NULL
-  }
-}
-
 github_resolve_ref.github_pull <- function(x, params) {
   # GET /repos/:user/:repo/pulls/:number
   path <- file.path("repos", params$username, params$repo, "pulls", x)
@@ -208,7 +183,7 @@ github_resolve_ref.github_release <- function(x, param) {
   list(ref = response[[1L]]$tag_name)
 }
 
-# Parse concise git repo specification: [username/]repo[/subdir][#pull|@ref|*release]
+# Parse concise git repo specification: [username/]repo[/subdir][#pull|@ref|@*release]
 # (the *release suffix represents the latest release)
 parse_git_repo <- function(path) {
   username_rx <- "(?:([^/]+)/)?"
