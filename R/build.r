a02b7222380f63e34dc6e3bbdab222a1c6abc8e7
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-06 10:35:13 +0200

    don't pass --no-manual if args contains --manual

--- R/build.r
+++ R/build.r
@@ -42,7 +42,9 @@ build <- function(pkg = ".", path = NULL, binary = FALSE, vignettes = TRUE,
       paste0(args, collapse = " "))
     ext <- if (.Platform$OS.type == "windows") "zip" else "tgz"
   } else {
-    args <- c(args, "--no-manual", "--no-resave-data")
+    if (!("--manual" %in% args))
+      args <- c(args, "--no-manual")
+    args <- c(args, "--no-resave-data")
 
     if (!vignettes) {
       args <- c(args, "--no-build-vignettes")
