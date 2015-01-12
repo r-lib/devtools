45bd7ad9bf39a4b8f0bacd194809fe6d591d7903
hadley <h.wickham@gmail.com>
2015-01-12 14:56:49 -0600

    Add note about with_libpaths to install.
    Closes #670

--- R/install.r
+++ R/install.r
@@ -13,6 +13,8 @@
 #' If the package is loaded, it will be reloaded after installation. This is
 #' not always completely possible, see \code{\link{reload}} for caveats.
 #'
+#' To install a package in a non-default library, use \code{\link{with_libpaths}}.
+#'
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
 #' @param reload if \code{TRUE} (the default), will automatically reload the
