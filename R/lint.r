22132462c3c7fbb28184189caa1782036f8dd8e3
Jim Hester <james.f.hester@gmail.com>
2015-01-27 14:24:53 -0500

    Add cache argument for lint function and default to TRUE

--- R/lint.r
+++ R/lint.r
@@ -5,15 +5,17 @@
 #' override any or all of them using the \code{linters} parameter.
 #' @param pkg package description, can be path or package name. See
 #'   \code{\link{as.package}} for more information
+#' @param cache store the lint results so repeated lints of the same content
+#' use the previous results.
 #' @param ... additional arguments passed to \code{\link[lintr]{lint_package}}
 #' @seealso \code{\link[lintr]{lint_package}}, \code{\link[lintr]{lint}}
 #' @export
-lint <- function(pkg = ".", ...) {
+lint <- function(pkg = ".", cache = TRUE, ...) {
   check_lintr()
   pkg <- as.package(pkg)
 
   message("Linting ", pkg$package, appendLF = FALSE)
-  lintr::lint_package(pkg$path, ...)
+  lintr::lint_package(pkg$path, cache = TRUE, ...)
 }
 
 check_lintr <- function() {
