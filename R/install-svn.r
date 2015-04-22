f14507d3800495e17c0b13b60d5807b068c533bd
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2015-04-22 11:24:33 -0400

    Switched tryCatch to on.exit, throw error on failed switch to revision

--- R/install-svn.r
+++ R/install-svn.r
@@ -71,11 +71,12 @@ remote_download.svn_remote <- function(x, quiet = FALSE) {
   }
 
   if (!is.null(x$revision)) {
-    pwd <- getwd();
-    tryCatch ({
-      setwd(bundle);
-      request <- system2(svn_binary_path, paste('update -r', x$revision));
-    }, finally = setwd(pwd));
+    pwd <- getwd()
+	on.exit(setwd(pwd))
+	request <- system2(svn_binary_path, paste('update -r', x$revision))
+    if (request > 0) {
+      stop("There was a problem switching to the requested SVN revision", call. = FALSE)
+	}
   }
 
   bundle
