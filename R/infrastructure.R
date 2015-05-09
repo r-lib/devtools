65695b89f568040c983096dcca69170e73b7531e
tj mahr <tjmahr@users.noreply.github.com>
2015-05-09 12:43:03 -0500

    send a path to uses_git instead of a package object

--- R/infrastructure.R
+++ R/infrastructure.R
@@ -485,7 +485,7 @@ use_readme_rmd <- function(pkg = ".") {
   use_build_ignore("README.Rmd", pkg = pkg)
   use_build_ignore("^README-.*\\.png$", escape = FALSE, pkg = pkg)
 
-  if (uses_git(pkg) && file.exists(pkg$path, ".git", "hooks", "pre-commit")) {
+  if (uses_git(pkg$path) && file.exists(pkg$path, ".git", "hooks", "pre-commit")) {
     message("Adding pre-commit hook")
     use_git_hook("pre-commit", render_template("readme-rmd-pre-commit.sh"),
       pkg = pkg)
