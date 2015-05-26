5b14256eabcf0c27e0f1ec43dbbe443c0082ec23
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-26 13:31:56 +0200

    fix test: load package, and not R subdirectory

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -2,9 +2,9 @@ context('package')
 
 test_that("it can load from outside of package root", {
   expect_false('testHooks' %in% loadedNamespaces())
-  load_all(file.path("testHooks", "R"))
+  load_all(file.path("testHooks"))
   expect_true('testHooks' %in% loadedNamespaces())
-  unload(file.path("testHooks", "R"))
+  unload(file.path("testHooks"))
   expect_false('testHooks' %in% loadedNamespaces())
 })
 
