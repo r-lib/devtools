092482c20d0ddc9d3d4392814e63ef50214e08ed
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-26 13:50:22 +0200

    clarify docs, add test

--- tests/testthat/test-load.r
+++ tests/testthat/test-load.r
@@ -1,5 +1,10 @@
-context("Working directory when loading")
+context("Loading")
 
 test_that("Package root is working directory when loading", {
   expect_message(load_all("testLoadDir"), "[|].*/testLoadDir[|]")
+})
+
+test_that("Loading a package subdirectory throws an error", {
+  expect_error(load_all(file.path("testLoadDir", "R")),
+               "does not look like a package")
 })
