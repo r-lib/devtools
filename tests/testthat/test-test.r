54e681532152f0efeff007d3e80eada6b27b7c89
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-26 11:42:53 +0200

    add test that runs test() after removing testthat from search path

---
+++ tests/testthat/test-test.r
@@ -0,0 +1,11 @@
+context("Test")
+
+test_that("Package can be tested with testthat not on search path", {
+  testthat_pos <- which(search() == "package:testthat")
+  if (length(testthat_pos) > 0) {
+    testthat_env <- detach(pos = testthat_pos)
+    on.exit(attach(testthat_env, testthat_pos), add = TRUE)
+  }
+
+  test("testTest")
+})
