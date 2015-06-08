9cdeb6c5c73376c70610c53060827fa8a89b8735
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-08 16:01:50 +0200

    simple test for check_failures function

--- tests/testthat/test-check.r
+++ tests/testthat/test-check.r
@@ -1,5 +1,14 @@
 context("Check")
 
+test_that("return messages", {
+  on.exit(unlink("testHelp.Rcheck", recursive = TRUE), add = TRUE)
+  check("testHelp", document = FALSE, check_dir = ".", cleanup = FALSE, quiet = TRUE)
+
+  failures <- check_failures("testHelp.Rcheck", error = TRUE,
+                             warning = TRUE, note = TRUE)
+  expect_equal(failures, character())
+})
+
 test_that("aspell environment variables", {
   with_mock(
     `utils:::aspell_find_program` = function (...) "/bin/aspell",
