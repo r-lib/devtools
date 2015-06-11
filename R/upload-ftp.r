8d425cad9892024b23c95e8265cbc9a6dfd365f8
Jeroen Ooms <jeroenooms@gmail.com>
2015-06-11 16:55:06 +0200

    Add support for verbose FTP upload

--- R/upload-ftp.r
+++ R/upload-ftp.r
@@ -1,6 +1,6 @@
 #' @param file path to file on disk
 #' @param url full path of the ftp destination, including the filename
-upload_ftp <- function(file, url){
+upload_ftp <- function(file, url, verbose = FALSE){
   stopifnot(file.exists(file))
   stopifnot(is.character(url))
   con <- file(file, open = "rb")
@@ -8,6 +8,6 @@ upload_ftp <- function(file, url){
   h <- curl::new_handle(upload = TRUE, filetime = FALSE)
   curl::handle_setopt(h, readfunction = function(n){
     readBin(con, raw(), n = n)
-  })
+  }, verbose = verbose)
   curl::curl_fetch_memory(url, handle = h)
 }
