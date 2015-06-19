5e243239595fc5cb999767ad0eaca754ca40f051
hadley <h.wickham@gmail.com>
2015-06-19 10:59:50 -0500

    Update revdep check script

--- revdep/check.R
+++ revdep/check.R
@@ -1,7 +1,4 @@
 library("devtools")
 
-# Suggesting devtools probably just means that they're using it for
-# development, so we're probably safe skipping them.
 res <- revdep_check()
-
 revdep_check_save_summary(res)
