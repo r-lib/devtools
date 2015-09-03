12b6c391cd357c244cfd6830a4aec76b67a4635c
hadley <h.wickham@gmail.com>
2015-09-03 11:03:21 -0500

    Remove test

--- tests/testthat/test-check.r
+++ tests/testthat/test-check.r
@@ -20,14 +20,3 @@ test_that("return messages", {
                              warning = TRUE, note = TRUE)
   expect_equal(failures, character(), info = paste(failures, collapse = "\n"))
 })
-
-test_that("aspell environment variables", {
-  with_mock(
-    `utils:::aspell_find_program` = function (...) "/bin/aspell",
-    expect_equal(names(aspell_env_var()), "_R_CHECK_CRAN_INCOMING_USE_ASPELL_")
-  )
-  with_mock(
-    `utils:::aspell_find_program` = function (...) NA,
-    expect_warning(aspell_env_var(), "Skipping spell check")
-  )
-})
