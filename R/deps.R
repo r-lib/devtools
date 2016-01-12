f5a539bd4f5301da718e12a81bb3756bd550119a
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-12 13:51:35 +0000

    Fix dependency install when not upgrading
    It was broken.

--- R/deps.R
+++ R/deps.R
@@ -235,7 +235,7 @@ update.package_deps <- function(object, ..., quiet = FALSE, upgrade = TRUE) {
   if (upgrade) {
     behind <- object$package[object$diff < 0L]
   } else {
-    behind <- object$package[is.na(object$available)]
+    behind <- object$package[is.na(object$installed)]
   }
   if (length(behind) > 0L) {
     install_packages(behind, repos = attr(object, "repos"),
