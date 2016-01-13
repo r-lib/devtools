513a474ead968b1585ba48471741803162cea461
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-13 09:30:18 +0100

    using tryCatch() instead of try()

--- R/revdep-summarise.R
+++ R/revdep-summarise.R
@@ -17,9 +17,11 @@ revdep_check_save_logs <- function(res, log_dir = "revdep") {
 
     file.copy(file.path(path, logs), file.path(out, logs))
 
-    try({
+    tryCatch({
       desc <- check_description(path)
       write_dcf(file.path(out, "DESCRIPTION"), desc[c("Package", "Version", "Maintainer")])
+    }, error = function(e) {
+      message("Error checking DESCRIPTION for ", pkg, ": ", e$message)
     })
   }
 
@@ -67,11 +69,10 @@ revdep_check_summary <- function(res) {
 }
 
 try_check_summary_package <- function(path) {
-  res <- try(check_summary_package(path), silent = TRUE)
-  if (inherits(res, "try-error")) {
-    res <- as.character(res)
-  }
-  res
+  res <- tryCatch(
+    check_summary_package(path),
+    error = function(e) e$message
+  )
 }
 
 check_summary_package <- function(path) {
