2f22f86d1b7de79a14781337ef3408d5d06c363c
Robert M Flight <r.flight79@gmail.com>
2016-01-14 16:27:13 -0500

    add tests for using an option devtools.git.wd.clean

--- tests/testthat/test-github-connections.R
+++ tests/testthat/test-github-connections.R
@@ -155,6 +155,13 @@ test_that("git_wd_clean works properly", {
   expect_warning(git_wd_clean(test_pkg))
   expect_error(git_wd_clean(test_pkg, level = "die"))
 
+  options(devtools.git.wd.clean = "none")
+  expect_silent(git_wd_clean(test_pkg))
+  options(devtools.git.wd.clean = "warn")
+  expect_warning(git_wd_clean(test_pkg))
+  options(devtools.git.wd.clean = "die")
+  expect_error(git_wd_clean(test_pkg))
+
   erase(test_pkg)
 })
 
