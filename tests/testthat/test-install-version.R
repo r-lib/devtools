d2060d03a4d454d65be0b6eb244a9a70a5df6976
Jim Hester <james.f.hester@gmail.com>
2016-01-15 16:04:07 -0500

    travis' R doesn't have libcurl support currently

--- tests/testthat/test-install-version.R
+++ tests/testthat/test-install-version.R
@@ -2,12 +2,12 @@ context("Install specific version")
 
 test_that("package_find_repo() works correctly with multiple repos", {
 
-  repos <- c(CRANextras = "https://www.stats.ox.ac.uk/pub/RWin", CRAN = "https://cran.rstudio.com")
+  repos <- c(CRANextras = "http://www.stats.ox.ac.uk/pub/RWin", CRAN = "http://cran.rstudio.com")
   # ROI.plugin.glpk is the smallest package in the CRAN archive
   package <- "ROI.plugin.glpk"
   res <- package_find_repo(package, repos = repos)
 
   expect_equal(NROW(res), 1L)
-  expect_equal(res$repo, "https://cran.rstudio.com")
+  expect_equal(res$repo, "http://cran.rstudio.com")
   expect_match(rownames(res), package)
 })
