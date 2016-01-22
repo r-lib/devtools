cd23e7c7e38839e5502898bfb90bfd0db8d8e177
onlymee <antony@onlymee.co.uk>
2016-01-22 01:39:27 +0000

    Fix parameter code style

--- R/install-git.r
+++ R/install-git.r
@@ -1,6 +1,6 @@
 #' Install a package from a git repository
 #'
-#' It is vectorised so you can install multiple packages with 
+#' It is vectorised so you can install multiple packages with
 #' a single command. You do not need to have git installed.
 #'
 #' @param url Location of package. The url should point to a public or
@@ -8,7 +8,7 @@
 #' @param subdir A sub-directory within a git repository that may
 #'   contain the package we are interested in installing.
 #' @param branch Name of branch or tag to use, if not master.
-#' @param credentials A git2r credentials object passed through 
+#' @param credentials A git2r credentials object passed through
 #'   to \code{\link[git2r]{clone}}.
 #' @param args DEPRECATED. A character vector providing extra arguments to
 #'   pass on to git.
@@ -20,7 +20,7 @@
 #' install_git("git://github.com/hadley/stringr.git")
 #' install_git("git://github.com/hadley/stringr.git", branch = "stringr-0.2")
 #'}
-install_git <- function(url, subdir = NULL, branch = NULL, credentials=NULL, args = character(0),
+install_git <- function(url, subdir = NULL, branch = NULL, credentials = NULL, args = character(0),
                         ...) {
   if (!missing(args))
     warning("`args` is deprecated", call. = FALSE)
