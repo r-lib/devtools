96c86797a7d48dcabb0fd1d98c9ea3a49e1e781a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-02-12 17:25:28 +0100

    oops

--- R/build-dependencies.R
+++ R/build-dependencies.R
@@ -28,7 +28,7 @@ check_build_tools <- function(pkg = ".") {
 
 has_latex <- function(verbose = FALSE) {
   has <- nzchar(Sys.which("pdflatex"))
-  if (!has) {
+  if (!has && verbose) {
     message("pdflatex not found! Not building PDF manual or vignettes.\n",
             "If you are planning to release this package, please run a check with ",
             "manual and vignettes beforehand.\n")
