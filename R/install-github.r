dabe40636f4578adc43a7b93e925f40363fb9e2d
Nico <nampuero@enernoc.com>
2016-04-08 10:16:24 -0400

    "http://" option for github_has_remotes() like remote_download.github()

--- R/install-github.r
+++ R/install-github.r
@@ -98,8 +98,8 @@ github_remote <- function(repo, username = NULL, ref = NULL, subdir = NULL,
 #' @export
 remote_download.github_remote <- function(x, quiet = FALSE) {
   dest <- tempfile(fileext = paste0(".zip"))
-  if (!grepl("^http",x$host))
-    x$host <- paste0("https://",x$host) #default to https:// only if user has not specified explicitly
+  if (!grepl("^http", x$host))
+    x$host <- paste0("https://", x$host)
   src_root <- paste0(x$host, "/repos/", x$username, "/", x$repo)
   src <- paste0(src_root, "/zipball/", x$ref)
 
@@ -126,7 +126,9 @@ remote_download.github_remote <- function(x, quiet = FALSE) {
 }
 
 github_has_remotes <- function(x, auth = NULL) {
-  src_root <- paste0("https://", x$host, "/repos/", x$username, "/", x$repo)
+  if (!grepl("^http", x$host))
+    x$host <- paste0("https://", x$host)
+  src_root <- paste0(x$host, "/repos/", x$username, "/", x$repo)
   src_submodules <- paste0(src_root, "/contents/.gitmodules?ref=", x$ref)
   response <- httr::HEAD(src_submodules, , auth)
   identical(httr::status_code(response), 200L)
--- tests/testthat/infrastructure/revdep/check.R
+++
@@ -1,5 +0,0 @@
-library("devtools")
-
-revdep_check(threads = 6)
-revdep_check_save_summary()
-revdep_check_print_problems()
