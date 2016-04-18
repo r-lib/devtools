a9b332e1e29f9c3533037760a318a6f8bf3f2cde
Jim Hester <james.f.hester@gmail.com>
2016-04-18 15:02:18 -0400

    Database serialization changed for R < 3.2

--- tests/testthat/test-install-version.R
+++ tests/testthat/test-install-version.R
@@ -9,6 +9,9 @@ local_archive <- function(x) {
 
 test_that("package_find_repo() works correctly with multiple repos", {
 
+  # The archive format is not readable on older R versions
+  # (`do not know how to convert 'value' to class "POSIXct"`)
+  skip_if_not(getRversion() >= "3.2.0")
   repos <- c(CRANextras = "http://www.stats.ox.ac.uk/pub/RWin", CRAN = "http://cran.r-project.org")
   # ROI.plugin.glpk is the smallest package in the CRAN archive
   package <- "ROI.plugin.glpk"
@@ -25,6 +28,7 @@ test_that("package_find_repo() works correctly with multiple repos", {
 test_that("package_find_repo() works correctly with archived packages", {
   # Issue 1033
 
+  skip_if_not(getRversion() >= "3.2.0")
   repos <- c(CRAN = "http://cran.r-project.org")
   package <- "igraph0"
   with_mock(`devtools:::read_archive` = local_archive,
