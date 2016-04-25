975cc29c9a4f5661ea2a38dcb64bbf1cdd09bb10
Imanuel Costigan <i.costigan@me.com>
2016-04-25 18:25:46 +1000

    Copy parse_git_repo from install_github.R

--- R/install-bitbucket.r
+++ R/install-bitbucket.r
@@ -32,6 +32,38 @@ install_bitbucket <- function(repo, username = NULL, ref = "master", subdir = NU
   install_remotes(remotes, quiet = quiet, ...)
 }
 
+# Copy of code from install_github.R
+parse_git_repo <- function(path) {
+  username_rx <- "(?:([^/]+)/)?"
+  repo_rx <- "([^/@#]+)"
+  subdir_rx <- "(?:/([^@#]*[^@#/]))?"
+  ref_rx <- "(?:@([^*].*))"
+  pull_rx <- "(?:#([0-9]+))"
+  release_rx <- "(?:@([*]release))"
+  ref_or_pull_or_release_rx <- sprintf("(?:%s|%s|%s)?", ref_rx, pull_rx, release_rx)
+  github_rx <- sprintf("^(?:%s%s%s%s|(.*))$",
+    username_rx, repo_rx, subdir_rx, ref_or_pull_or_release_rx)
+
+  param_names <- c("username", "repo", "subdir", "ref", "pull", "release", "invalid")
+  replace <- stats::setNames(sprintf("\\%d", seq_along(param_names)), param_names)
+  params <- lapply(replace, function(r) gsub(github_rx, r, path, perl = TRUE))
+  if (params$invalid != "")
+    stop(sprintf("Invalid git repo: %s", path))
+  params <- params[sapply(params, nchar) > 0]
+
+  if (!is.null(params$pull)) {
+    params$ref <- github_pull(params$pull)
+    params$pull <- NULL
+  }
+
+  if (!is.null(params$release)) {
+    params$ref <- github_release()
+    params$release <- NULL
+  }
+
+  params
+}
+
 bitbucket_remote <- function(repo, username = NULL, ref = NULL, subdir = NULL,
                               auth_user = NULL, password = NULL, sha = NULL) {
 
