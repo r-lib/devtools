6e0af125103e7619eccf2a2ce8fe49bbc2abd387
Jim Hester <james.f.hester@gmail.com>
2016-04-27 10:50:11 -0400

    Only clean def files if they are 'packageName.def'

--- R/compile-dll.r
+++ R/compile-dll.r
@@ -51,7 +51,7 @@ clean_dll <- function(pkg = ".") {
 
   # Clean out the /src/ directory
   files <- dir(file.path(pkg$path, "src"),
-    pattern = "\\.(o|sl|so|dylib|a|dll|def)$",
+    pattern = sprintf("\\.(o|sl|so|dylib|a|dll)$|(%s\\.def)$", pkg$package),
     full.names = TRUE)
   unlink(files)
 
