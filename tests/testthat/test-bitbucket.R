977e49c9032850a7bcea1ca0d49f2fbd03f44c8d
Imanuel Costigan <i.costigan@me.com>
2016-05-14 13:48:46 +1000

    Skip bitbucket_remote tests on remotes

--- tests/testthat/test-bitbucket.R
+++ tests/testthat/test-bitbucket.R
@@ -24,6 +24,11 @@ test_that("Bitbucket repos parsed correctly", {
 })
 
 test_that("Bitbucket remote build works", {
+  # bitbucket_remote requires the OAuth token to be available in $HOME. So
+  # skip these tests on CI plaforms / CRAN.
+  skip_on_travis()
+  skip_on_appveyor()
+  skip_on_cran()
   # Mock resolve_ref so that Bitbucket API is not queried for this test
   mock_bitbucket_resolve_ref.bitbucket_pull <- function(x, params) {
     params$username <- sprintf("user-%s", x)
