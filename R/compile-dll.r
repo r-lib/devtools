0a08764a95a0b65e72c175002d1cb0f814d05c14
Jim Hester <james.f.hester@gmail.com>
2016-05-31 14:44:37 -0400

    Clean files recursively
    Fixes #1175

--- R/compile-dll.r
+++ R/compile-dll.r
@@ -52,7 +52,8 @@ clean_dll <- function(pkg = ".") {
   # Clean out the /src/ directory
   files <- dir(file.path(pkg$path, "src"),
     pattern = sprintf("\\.(o|sl|so|dylib|a|dll)$|(%s\\.def)$", pkg$package),
-    full.names = TRUE)
+    full.names = TRUE,
+    recursive = TRUE)
   unlink(files)
 
   invisible(files)
