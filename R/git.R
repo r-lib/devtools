f0b9cc9b37f2ff8f294754a34088c104a2f6c0ab
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-06-06 10:13:40 -0500

    simplifies regular expressions

--- R/git.R
+++ R/git.R
@@ -135,10 +135,10 @@ github_remote_parse <- function(x) {
 
   if (grepl("^https", x)) {
     # https://github.com/hadley/devtools.git
-    re <- "https://[^/]*github[^/]*/(.*?)/(.*)\\.git"
+    re <- "github[^/]*/(.*?)/(.*)\\.git"
   } else if (grepl("^git", x)) {
     # git@github.com:hadley/devtools.git
-    re <- "git@[^:]*github[^:]*:(.*?)/(.*)\\.git"
+    re <- "github[^:]*:(.*?)/(.*)\\.git"
   } else {
     stop("Unknown GitHub repo format", call. = FALSE)
   }
