af11ad7f6f0f4da2b45cfa1d09a4ca9587e4c262
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-06-08 13:56:23 -0500

    simplifies regex & logic in github_remote_parse()

--- R/git.R
+++ R/git.R
@@ -133,12 +133,10 @@ github_remote_parse <- function(x) {
   if (length(x) == 0) return(github_dummy)
   if (!grepl("github", x)) return(github_dummy)
 
-  if (grepl("^https", x)) {
+  if (grepl("^(https|git)", x)) {
     # https://github.com/hadley/devtools.git
-    re <- "github[^/]*/(.*?)/(.*)\\.git"
-  } else if (grepl("^git", x)) {
     # git@github.com:hadley/devtools.git
-    re <- "github[^:]*:(.*?)/(.*)\\.git"
+    re <- "github[^/:]*[/:](.*?)/(.*)\\.git"
   } else {
     stop("Unknown GitHub repo format", call. = FALSE)
   }
