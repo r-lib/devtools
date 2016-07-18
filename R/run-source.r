386fef1f0187c3322f0a1d5d786b873ead180749
Adam Smith <avianmigration@gmail.com>
2016-07-18 14:46:33 -0400

    Source single file gist without filename; fixes #1266
    This fixes an issue in which an attempt to source a gist containing a single filename, but without specifying that filename, fails.  Fixes issue #1266.

--- R/run-source.r
+++ R/run-source.r
@@ -137,8 +137,8 @@ find_gist <- function(id, filename) {
     }
 
   } else {
-    if (length(r_files) > 1) {
-      warning("Multiple R files in gist, using first.")
+    if (length(r_files) >= 1) {
+      if (length(r_files) > 1) warning("Multiple R files in gist, using first.")
       which <- 1
     }
   }
