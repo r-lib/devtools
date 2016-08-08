3546faad306f43982c5e302e38d1d3a84c94579a
Jennifer (Jenny) Bryan <jenny@stat.ubc.ca>
2016-08-08 06:10:31 -0700

    Take the best of both README templates (#1287)
    * Use correct path in open_in_rstudio()
    
    * Unified README template w/ sections for overview, installation, example
    
    * add bullet to NEWS.md

--- R/infrastructure.R
+++ R/infrastructure.R
@@ -466,18 +466,36 @@ use_build_ignore <- function(files, escape = TRUE, pkg = ".") {
 
 #' Create README files.
 #'
+#' Creates skeleton README files with sections for
+#' \itemize{
+#' \item a high-level description of the package and its goals
+#' \item R code to install from GitHub, if GitHub usage detected
+#' \item a basic example
+#' }
 #' Use \code{Rmd} if you want a rich intermingling of code and data. Use
 #' \code{md} for a basic README. \code{README.Rmd} will be automatically
-#' added to \code{.Rbuildignore}.
+#' added to \code{.Rbuildignore}. The resulting README is populated with default
+#' YAML frontmatter and R fenced code blocks (\code{md}) or chunks (\code{Rmd}).
 #'
 #' @param pkg package description, can be path or package name.  See
 #'   \code{\link{as.package}} for more information
 #' @export
+#' @examples
+#' \dontrun{
+#' use_readme_rmd()
+#' use_readme_md()
+#' }
 #' @family infrastructure
 use_readme_rmd <- function(pkg = ".") {
   pkg <- as.package(pkg)
 
-  use_template("README.Rmd", ignore = TRUE, open = TRUE, pkg = pkg)
+  if (uses_github(pkg$path)) {
+    pkg$github <- github_info(pkg$path)
+  }
+  pkg$Rmd <- TRUE
+
+  use_template("omni-README", save_as = "README.Rmd", data = pkg,
+               ignore = TRUE, open = TRUE, pkg = pkg)
   use_build_ignore("^README-.*\\.png$", escape = FALSE, pkg = pkg)
 
   if (uses_git(pkg$path) && !file.exists(pkg$path, ".git", "hooks", "pre-commit")) {
@@ -497,7 +515,8 @@ use_readme_md <- function(pkg = ".") {
     pkg$github <- github_info(pkg$path)
   }
 
-  use_template("README.md", data = pkg, open = TRUE, pkg = pkg)
+  use_template("omni-README", save_as = "README.md",
+               data = pkg, open = TRUE, pkg = pkg)
 }
 
 #' Use NEWS.md
@@ -724,7 +743,7 @@ use_template <- function(template, save_as = template, data = list(),
 
   if (open) {
     message("* Modify `", save_as, "`.")
-    open_in_rstudio(save_as)
+    open_in_rstudio(path)
   }
 
   invisible(TRUE)
