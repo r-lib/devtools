r-lib/devtools

commit 7f54aaf4c9f2b31d498d096d98004d4a906b8fb5

cache result of remote_deps() so we don't query it twice

R/deps.R
@@ -111,9 +111,15 @@ dev_package_deps <- function(pkg = ".", dependencies = NA,
       repos[missing_repos] <- bioc_repos[missing_repos]
   }
 
-  filter_duplicate_deps(
+  res <- filter_duplicate_deps(
     package_deps(deps, repos = repos, type = type),
-    remote_deps(pkg, deps))
+
+    # We set this cache in install() so we can run install_deps() twice without
+    # having to re-query the remotes
+    installing$remote_deps %||% remote_deps(pkg))
+
+  # Only keep dependencies we actually want to use
+  res[res$package %in% deps, ]
 }
 
 filter_duplicate_deps <- function(cran_deps, remote_deps, dependencies) {
@@ -186,7 +192,7 @@ split_remotes <- function(x) {
   trim_ws(unlist(strsplit(x, ",[[:space:]]*")))
 }
 
-remote_deps <- function(pkg, deps) {
+remote_deps <- function(pkg) {
   pkg <- as.package(pkg)
 
   if (!has_dev_remotes(pkg)) {
@@ -213,7 +219,7 @@ remote_deps <- function(pkg, deps) {
     class = c("package_deps", "data.frame"))
   res$remote <- structure(remotes, class = "remotes")
 
-  res[res$package %in% deps, ]
+  res
 }
 
 has_dev_remotes <- function(pkg) {

R/install.r
@@ -104,6 +104,11 @@ install <-
   initial_deps <- dependencies[dependencies != "Suggests"]
   final_deps <- dependencies[dependencies == "Suggests"]
 
+  # cache the Remote: dependencies here so we don't have to query them each
+  # time we call install_deps
+  installing$remote_deps <- remote_deps(pkg)
+  on.exit(installing$remote_deps <- NULL, add = TRUE)
+
   install_deps(pkg, dependencies = initial_deps, upgrade = upgrade_dependencies,
     threads = threads, force_deps = force_deps, quiet = quiet, ...)
 
@@ -113,7 +118,7 @@ install <-
     built_path <- pkg$path
   } else {
     built_path <- build(pkg, tempdir(), vignettes = build_vignettes, quiet = quiet)
-    on.exit(unlink(built_path))
+    on.exit(unlink(built_path), add = TRUE)
   }
 
   opts <- c(

commit 1ce84b04568ff7846c3da754f28e7e22a23c8737

Do not filter dependencies by those listed in DESCRIPTION
The idea of filtering was to prevent listing a dependency in Remotes:
without also listing it somewhere else in your DESCRIPTION. The
filtering was previously only done on remote dependencies,
7f54aaf4c9f2b31d498d096d98004d4a906b8fb5 inadvertently changed the
filtering for all dependencies which caused the bug.

Thinking about this some more I think we should just remove this
filtering altogether. There can be times you want to install something
in Remotes without actually taking a dependency on it. (covr or lintr
for instance)

R/deps.R
@@ -115,15 +115,12 @@ dev_package_deps <- function(pkg = ".", dependencies = NA,
       repos[missing_repos] <- bioc_repos[missing_repos]
   }
 
-  res <- filter_duplicate_deps(
+  filter_duplicate_deps(
     package_deps(deps, repos = repos, type = type),
 
     # We set this cache in install() so we can run install_deps() twice without
     # having to re-query the remotes
     installing$remote_deps %||% remote_deps(pkg))
-
-  # Only keep dependencies we actually want to use
-  res[res$package %in% deps, ]
 }
 
 filter_duplicate_deps <- function(cran_deps, remote_deps, dependencies) {