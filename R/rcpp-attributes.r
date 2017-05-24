7c18914e44fc9784c000ef522533524e47b953cd
Jim Hester <james.f.hester@gmail.com>
2017-05-24 08:12:48 -0400

    Workaround for https://github.com/RcppCore/Rcpp/pull/697

--- R/rcpp-attributes.r
+++ R/rcpp-attributes.r
@@ -6,7 +6,12 @@ compile_rcpp_attributes <- function(pkg) {
   # Only scan for attributes in packages explicitly linking to Rcpp
   if (links_to_rcpp(pkg)) {
     check_suggested("Rcpp")
-    Rcpp::compileAttributes(pkg$path)
+
+    # We need to use use with_dir here to work around a regression in Rcpp
+    # 0.12.11 (https://github.com/RcppCore/Rcpp/pull/697)
+    withr_with_dir(pkg$path, {
+      Rcpp::compileAttributes()
+    })
   }
 }
 
