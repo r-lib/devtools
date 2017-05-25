9a373b78f5bf347dee0bcba3ab0e7fec03598445
Jim Hester <james.f.hester@gmail.com>
2017-05-25 07:58:23 -0700

    Fixes to get test working with R 3.3.3

--- tests/testthat/testDllRcpp/R/rcpp_hello_world.R
+++ tests/testthat/testDllRcpp/R/rcpp_hello_world.R
@@ -1,5 +1,5 @@
 
 rcpp_hello_world <- function(){
-	.Call( "rcpp_hello_world", PACKAGE = "testDllRcpp" )
+	.Call("testDllRcpp_rcpp_hello_world", PACKAGE = "testDllRcpp" )
 }
 
--- tests/testthat/testDllRcpp/src/rcpp_hello_world.cpp
+++ tests/testthat/testDllRcpp/src/rcpp_hello_world.cpp
@@ -1,16 +1,15 @@
-#include "rcpp_hello_world.h"
+#include <Rcpp.h>
 
-SEXP rcpp_hello_world(){
-    using namespace Rcpp ;
-    
-    CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
-    NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
-    List z            = List::create( x, y ) ;
-    
-    return z ;
+// [[Rcpp::export]]
+SEXP rcpp_hello_world() {
+  using namespace Rcpp;
+
+  CharacterVector x = CharacterVector::create("foo", "bar");
+  NumericVector y = NumericVector::create(0.0, 1.0);
+  List z = List::create(x, y);
+
+  return z;
 }
 
 // [[Rcpp::export]]
-bool rcpp_test_attributes() {
-  return true;
-}
+bool rcpp_test_attributes() { return true; }
