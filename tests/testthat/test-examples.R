ba1002376620a1b07a51b89dc682c8c03b61409d
Andrew McDavid <amcdavid@fhcrc.org>
2017-07-19 13:18:23 -0400

    simplify expect_output to avoid special characters

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -2,5 +2,5 @@ context('Examples')
 
 test_that('Can run an example', {
     pkg <- "testHelp"
-    expect_output(run_examples(pkg=pkg), "stopifnot(foofoo()=='You called foofoo.')", fixed=TRUE)
+    expect_output(run_examples(pkg=pkg), "You called foofoo.", fixed=TRUE)
     })
