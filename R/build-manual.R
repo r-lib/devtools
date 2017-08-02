4c6f9bdba8d4bbbfcf0c1efd01991cb41315012d
Tymoteusz Wołodźko <twolodzko@gmail.com>
2017-08-02 23:30:26 +0200

    build_manual function

---
+++ R/build-manual.R
@@ -0,0 +1,19 @@
+
+#' Create package pdf manual
+#'
+#' @param pkg package description, can be path or package name.  See
+#'   \code{\link{as.package}} for more information.
+#' @param path path in which to produce package manual.
+#'   If \code{NULL}, defaults to the parent directory of the package.
+#'
+#' @seealso \code{\link{RdUtils}}
+#' @export
+
+build_manual <- function(pkg = ".", path = NULL) {
+  pkg <- as.package(pkg)
+  if (is.null(path)) {
+    path <- dirname(pkg$path)
+  }
+  name <- paste0(pkg$package, "_", pkg$version, ".pdf", collapse = " ")
+  system(paste0("R CMD Rd2pdf --force --output=", path, "/", name, " ", shQuote(pkg$path), collapse = " "))
+}
