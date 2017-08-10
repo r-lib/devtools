751aec1eb81994a8b6d200ae7f655206fe77a370
Neil-Schneider <Schneider.Neil@gmail.com>
2017-08-10 14:28:52 -0400

    Add leading characters to expected output
    Do this because the test fails because `\n>` is captured by the function in this test environment. Not sure it is a platform issue, but on my windows machine both tests, with and without `\n>` pass.

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -3,5 +3,5 @@ context('Examples')
 test_that('Can run an example', {
     pkg <- "testHelp"
     expect_output(run_examples(pkg = pkg)
-      , "stopifnot(foofoo() == 'You called foofoo.')", fixed = TRUE)
+      , "\n> stopifnot(foofoo() == 'You called foofoo.')", fixed = TRUE)
     })
