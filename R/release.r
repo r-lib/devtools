13ee56b9bb43dd2cb2e3b364bcba2c49ef350fac
Jim Hester <james.f.hester@gmail.com>
2018-04-09 12:31:13 -0400

    Fix typo

--- R/release.r
+++ R/release.r
@@ -112,7 +112,7 @@ release <- function(pkg = ".", check = FALSE, args = NULL) {
     "Have you updated `NEWS.md` file?",
     "Have you updated `DESCRIPTION`?",
     "Have you updated `cran-comments.md?`",
-    if (dir.exists("docs/")) "Have use updated website in `docs/`?",
+    if (dir.exists("docs/")) "Have you updated website in `docs/`?",
     find_release_questions()
   )
   for (question in questions) {
