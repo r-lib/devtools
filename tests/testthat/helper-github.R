bba8bff7bd0663bfd27a17080bf04be8e5ef1dca
Jim Hester <james.f.hester@gmail.com>
2018-06-27 11:16:41 -0400

    Change location of devtools from hadley to r-lib

--- tests/testthat/helper-github.R
+++ tests/testthat/helper-github.R
@@ -13,7 +13,7 @@ erase <- function(path) unlink(path, recursive = TRUE)
 mock_use_github <- function(pkg) {
   use_git_with_config(message = "initial", pkg = pkg, add_user_config = TRUE, quiet = TRUE)
   r <- git2r::repository(pkg)
-  git2r::remote_add(r, "origin", "https://github.com/hadley/devtools.git")
+  git2r::remote_add(r, "origin", "https://github.com/r-lib/devtools.git")
   use_github_links(pkg)
   git2r::add(r, "DESCRIPTION")
   git2r::commit(r, "Add GitHub links to DESCRIPTION")
