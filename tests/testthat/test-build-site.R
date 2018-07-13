2b7ba7dfdecde9cebdb0443c9b971f4f52813469
Roy Storey <kiwiroy@users.noreply.github.com>
2018-07-13 14:52:23 +1200

    add build_site wrapper #1777

---
+++ tests/testthat/test-build-site.R
@@ -0,0 +1,25 @@
+context("Build Site")
+
+test_that("Package pkgdown site can be built ", {
+  destination <- file.path(tempdir(), "testPkgdown", "docs")
+
+  build_output <- capture.output({
+    build_site(path     = "./testPkgdown",
+               override = list(destination = destination))
+  }, type = c("output"))
+
+  build_output <- paste(build_output, collapse = "\n")
+
+  expect_true(file.exists(file.path(destination, "index.html")),
+              info  = build_output,
+              label = "created site index")
+  expect_true(file.exists(file.path(destination, "reference", "index.html")),
+              info  = build_output,
+              label = "created reference index")
+  expect_true(file.exists(file.path(destination, "articles", "index.html")),
+              info  = build_output,
+              label = "created articles index")
+  expect_true(file.exists(file.path(destination, "articles", "test.html")),
+              info  = build_output,
+              label = "created articles index")
+})
---
+++ tests/testthat/testPkgdown/R/pkgdown-test-test.R
@@ -0,0 +1,11 @@
+
+#' pkgdown_test_test
+#'
+#' @param x marks the spot
+#'
+#' @return FALSE
+#' @export
+#'
+pkgdown_test_test <- function(x) {
+  return(FALSE)
+}
