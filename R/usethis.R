719936bbbdc2db15e81ad020358011438fd6f130
Jim Hester <james.f.hester@gmail.com>
2018-07-20 12:50:47 -0400

    fixup! Use full argument name for upgrade_dependencies

--- R/usethis.R
+++ R/usethis.R
@@ -4,7 +4,7 @@
 local_proj <- withr::local_(usethis::proj_set)
 
 usethis_use_directory <- function(pkg, dir, ignore = FALSE) {
-  capture.output({
+  utils::capture.output({
     local_proj(pkg$path)
     usethis::use_directory(dir, ignore)
   })
