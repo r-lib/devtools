e8d84ebd92325b2490b2b5f72477d1b26c6553e2
Jim Hester <james.f.hester@gmail.com>
2018-08-14 08:46:56 -0400

    Fully qualify calls to capture.output

--- R/usethis.R
+++ R/usethis.R
@@ -4,14 +4,14 @@
 local_proj <- withr::local_(function(path = ".", force = FALSE, quiet = FALSE) utils::capture.output(usethis::proj_set(path = path, force = force, quiet = quiet)))
 
 usethis_use_directory <- function(pkg, path, ignore = FALSE) {
-  capture.output({
+  utils::capture.output({
     local_proj(pkg$path)
     usethis::use_directory(path, ignore)
   })
 }
 
 usethis_use_git_ignore <- function(pkg, ignores, ignore = FALSE) {
-  capture.output({
+  utils::capture.output({
     local_proj(pkg$path)
     usethis::use_git_ignore(ignores)
   })
