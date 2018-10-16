8145acc8558fab2f4a3b7e82183b09fc08b348d3
Jim Hester <james.f.hester@gmail.com>
2018-10-16 15:26:46 -0400

    Cleanup all generated files from test-build-readme

--- tests/testthat/test-build-readme.R
+++ tests/testthat/test-build-readme.R
@@ -3,7 +3,8 @@ context("Build README")
 test_that("Package readme can be built ", {
   destination <- file.path("testReadme", "README.md")
 
-  on.exit(unlink("testReadme/README.md"))
+  on.exit(unlink(c("testReadme/README.md", "testReadme/README.html", "testReadme/man/figures"), recursive = TRUE))
+
   build_readme("testReadme")
 
   expect_true(file.exists(destination))
