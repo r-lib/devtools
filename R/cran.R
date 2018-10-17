6b28a5d44dd2871eef48a5acae5afb7b7e88a42c
Jim Hester <james.f.hester@gmail.com>
2018-10-17 14:43:50 -0400

    Remove code no longer used by devtools

--- R/cran.R
+++ R/cran.R
@@ -1,28 +1,3 @@
-available_packages <- memoise::memoise(function(repos, type) {
-  available.packages(contrib.url(repos, type), type = type)
-})
-
-package_url <- function(package, repos,
-                        available = available_packages(repos, "source")) {
-  ok <- (available[, "Package"] == package)
-  ok <- ok & !is.na(ok)
-
-  if (!any(ok)) {
-    return(list(name = NA_character_, url = NA_character_))
-  }
-
-  vers <- package_version(available[ok, "Version"])
-  keep <- vers == max(vers)
-  keep[duplicated(keep)] <- FALSE
-  ok[ok][!keep] <- FALSE
-
-  name <- paste(package, "_", available[ok, "Version"], ".tar.gz", sep = "")
-  url <- file.path(available[ok, "Repository"], name)
-
-  list(name = name, url = url)
-}
-
-
 # Return the version of a package on CRAN (or other repository)
 # @param package The name of the package.
 # @param available A matrix of information about packages.
