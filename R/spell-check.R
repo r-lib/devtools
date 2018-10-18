fcade5bdc232dafd42eda4ed59a853d5a73f70bf
Jim Hester <james.f.hester@gmail.com>
2018-10-18 10:34:06 -0400

    Update documentation for spell_check

--- R/spell-check.R
+++ R/spell-check.R
@@ -3,18 +3,15 @@
 #' Runs a spell check on text fields in the package description file, manual
 #' pages, and optionally vignettes. Wraps the \link[spelling:spell_check_package]{spelling}
 #' package.
-#' manual pages. Hunspell includes dictionaries for `en_US` and `en_GB`
-#' by default. Other languages require installation of a custom dictionary, see
-#' the \href{https://cran.r-project.org/package=hunspell/vignettes/intro.html#system_dictionaries}{hunspell vignette}
-#' for details.
 #'
 #' @export
 #' @rdname spell_check
 #' @param pkg package description, can be path or package name.  See
 #'   [as.package()] for more information
-#' @param vignettes include vignettes in the spell check; passed to
-#' \link[spelling:spell_check_package]{spelling::spell_check_package}
-spell_check <- function(pkg = ".", vignettes = TRUE) {
+#' @param vignettes also check all `rmd` and `rnw` files in the pkg `vignettes` folder
+#' @param use_wordlist ignore words in the package [WORDLIST][spelling::get_wordlist] file
+#' @inheritParams spelling::spell_check_package
+spell_check <- function(pkg = ".", vignettes = TRUE, use_wordlist = TRUE) {
   pkg <- as.package(pkg)
-  spelling::spell_check_package(pkg = pkg, vignettes = vignettes)
+  spelling::spell_check_package(pkg = pkg, vignettes = vignettes, use_wordlist = TRUE)
 }
