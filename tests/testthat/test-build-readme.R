9a6cc60888e12dfd7e51e277262f3344683a5a74
Jim Hester <james.f.hester@gmail.com>
2019-01-03 08:41:59 -0500

    Skip readme test on CRAN
    Some CRAN machines (macOS and Solaris) don't have a new enough pandoc
    version, so tests fail with
    
    > Error: pandoc version 1.12.3 or higher is required and was not found
    (see the help page ?rmarkdown::pandoc_available).

--- tests/testthat/test-build-readme.R
+++ tests/testthat/test-build-readme.R
@@ -1,6 +1,8 @@
 context("Build README")
 
 test_that("Package readme can be built ", {
+  skip_on_cran()
+
   destination <- file.path("testReadme", "README.md")
 
   on.exit(unlink(c("testReadme/README.md", "testReadme/README.html", "testReadme/man/figures"), recursive = TRUE))
