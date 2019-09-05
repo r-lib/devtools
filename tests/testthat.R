2f5c166da76a43ecce30da30701c522a5c5e5185
Jim Hester <james.f.hester@gmail.com>
2019-09-05 08:40:08 -0400

    Revert "Add azure pipelines"
    This reverts commit 9e079e7aa464b5cc235ca19a578b6955887b4c61.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(devtools)
 
-if (requireNamespace("xml2")) {
-  test_check("devtools", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
-} else {
-  test_check("devtools")
-}
+test_check("devtools")
