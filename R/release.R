962c9239261295f8439f949440dab4f35726eae5
Hadley Wickham <h.wickham@gmail.com>
2020-07-22 14:00:05 -0500

    Move helper into release.R

--- R/release.R
+++ R/release.R
@@ -389,3 +389,15 @@ cran_mirror <- function(repos = getOption("repos")) {
 
   repos[["CRAN"]]
 }
+
+# Return the version of a package on CRAN (or other repository)
+# @param package The name of the package.
+# @param available A matrix of information about packages.
+cran_pkg_version <- function(package, available = available.packages()) {
+  idx <- available[, "Package"] == package
+  if (any(idx)) {
+    as.package_version(available[package, "Version"])
+  } else {
+    NULL
+  }
+}
