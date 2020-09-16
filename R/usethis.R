da96df6cb6fa87ebcf36454d059237a26d2a016d
Jim Hester <james.f.hester@gmail.com>
2020-09-16 10:53:22 -0400

    Return the result of the call, not the results of capture.output
    The latter is just the text that is output, which is not what we want.
    
    This was uncovered by a test failure in the next release of withr

--- R/usethis.R
+++ R/usethis.R
@@ -5,26 +5,33 @@
 #' @importFrom usethis use_test
 NULL
 
-#' @importFrom withr defer
-local_proj <- withr::local_(function(path = ".", force = FALSE) utils::capture.output(usethis::proj_set(path = path, force = force)))
+local_proj <- withr::local_(function(path = ".", force = FALSE) {
+  utils::capture.output(
+    res <- usethis::proj_set(path = path, force = force)
+  )
+  res
+})
 
 usethis_use_testthat <- function(pkg) {
   utils::capture.output({
     local_proj(pkg$path)
-    usethis::use_testthat()
+    res <- usethis::use_testthat()
   })
+  res
 }
 
 usethis_use_directory <- function(pkg, path, ignore = FALSE) {
   utils::capture.output({
     local_proj(pkg$path)
-    usethis::use_directory(path, ignore)
+    res <- usethis::use_directory(path, ignore)
   })
+  res
 }
 
 usethis_use_git_ignore <- function(pkg, ignores, ignore = FALSE) {
   utils::capture.output({
     local_proj(pkg$path)
-    usethis::use_git_ignore(ignores)
+    res <- usethis::use_git_ignore(ignores)
   })
+  res
 }
