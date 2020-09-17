d5a60ad5a7e7c6ae56375e63018ac79d37345c03
Jim Hester <james.f.hester@gmail.com>
2020-09-17 13:32:21 -0400

    Use local_project from usethis directly
    Thanks @jennybc

--- R/usethis.R
+++ R/usethis.R
@@ -5,35 +5,17 @@
 #' @importFrom usethis use_test
 NULL
 
-# Need to import defer because the local_ function uses it unqualified
-#' @importFrom withr defer
-local_proj <- withr::local_(function(path = ".", force = FALSE) {
-  utils::capture.output(
-    res <- usethis::proj_set(path = path, force = force)
-  )
-  res
-})
-
 usethis_use_testthat <- function(pkg) {
-  utils::capture.output({
-    local_proj(pkg$path)
-    res <- usethis::use_testthat()
-  })
-  res
+  usethis::local_project(pkg$path, quiet = TRUE)
+  usethis::use_testthat()
 }
 
 usethis_use_directory <- function(pkg, path, ignore = FALSE) {
-  utils::capture.output({
-    local_proj(pkg$path)
-    res <- usethis::use_directory(path, ignore)
-  })
-  res
+  usethis::local_project(pkg$path, quiet = TRUE)
+  usethis::use_directory(path, ignore)
 }
 
 usethis_use_git_ignore <- function(pkg, ignores, ignore = FALSE) {
-  utils::capture.output({
-    local_proj(pkg$path)
-    res <- usethis::use_git_ignore(ignores)
-  })
-  res
+  usethis::local_project(pkg$path, quiet = TRUE)
+  usethis::use_git_ignore(ignores)
 }
