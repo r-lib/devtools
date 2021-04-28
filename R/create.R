4025c0417c6bf602fa00258ffc7c91fc7607659d
Malcolm Barrett <malcolmbarrett@users.noreply.github.com>
2021-04-28 09:35:18 -0700

    Don't open projects with `create()` (#2347)

--- R/create.R
+++ R/create.R
@@ -3,8 +3,9 @@
 #' @param path A path. If it exists, it is used. If it does not exist, it is
 #'   created, provided that the parent path exists.
 #' @param ... Additional arguments passed to [usethis::create_package()]
+#' @inheritParams usethis::create_package
 #' @return The path to the created package, invisibly.
 #' @export
-create <- function(path, ...) {
-  usethis::create_package(path, ...)
+create <- function(path, ..., open = FALSE) {
+  usethis::create_package(path, ..., open = open)
 }
