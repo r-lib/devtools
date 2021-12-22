a999c84c03a8c955bf1bc81b2ae3f6d8e1c7110b
Romain François <romain@rstudio.com>
2021-12-22 04:47:20 +0100

    `devtools::test()` case when not `interactive()` and testing infrastructure not found (#2396)
    * devtools::test() case when not interactive and testing infracture not found.
    
    * .
    
    * Various changes
    
    * Use `yesno()` for internal consistency
    * If no pre-existing test infrastructure, exit even if we call `use_testthat()`, because there aren't any actual tests yet.
    * Let `use_testthat()` messages through.
    
    Co-authored-by: Jenny Bryan <jenny.f.bryan@gmail.com>

--- R/test.R
+++ R/test.R
@@ -22,11 +22,16 @@ test <- function(pkg = ".", filter = NULL, stop_on_failure = FALSE, export_all =
   save_all()
   pkg <- as.package(pkg)
 
-  if (!uses_testthat(pkg) && interactive()) {
-    cli::cli_alert_danger("No testing infrastructure found. Create it?")
-    if (utils::menu(c("Yes", "No")) == 1) {
-      usethis_use_testthat(pkg)
+  if (!uses_testthat(pkg)) {
+    cli::cli_alert_danger("No testing infrastructure found.")
+    if (!interactive()) {
+      ui_todo('Setup testing with {ui_code("usethis::use_testthat()")}.')
+      return(invisible())
     }
+    if (yesno("Create it?")) {
+      return(invisible())
+    }
+    usethis_use_testthat(pkg)
     return(invisible())
   }
 
