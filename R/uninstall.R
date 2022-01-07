6c4bd54ae04f17da9d500473feffc537b198a618
Kara Woo <karawoo@users.noreply.github.com>
2022-01-07 12:40:35 -0800

    Fix typo in documentation (#2401)
    * Use correct function name in documentation
    
    * More editing
    
    Co-authored-by: Jenny Bryan <jenny.f.bryan@gmail.com>

--- R/uninstall.R
+++ R/uninstall.R
@@ -1,17 +1,15 @@
-#' Uninstall a local development package.
+#' Uninstall a local development package
 #'
-#' Uses `remove.package` to uninstall the package.
-#' To uninstall a package from a non-default library,
-#' use [withr::with_libpaths()].
+#' Uses `remove.packages()` to uninstall the package. To uninstall a package
+#' from a non-default library, use in combination with [withr::with_libpaths()].
 #'
 #' @inheritParams install
-#' @param unload if `TRUE` (the default), will automatically unload the
-#'   package prior to uninstalling.
+#' @param unload if `TRUE` (the default), ensures the package is unloaded, prior
+#'   to uninstalling.
 #' @inheritParams utils::remove.packages
 #' @export
 #' @family package installation
-#' @seealso [with_debug()] to install packages with debugging flags
-#'   set.
+#' @seealso [with_debug()] to install packages with debugging flags set.
 uninstall <- function(pkg = ".", unload = TRUE, quiet = FALSE, lib = .libPaths()[[1]]) {
   pkg <- as.package(pkg)
 
