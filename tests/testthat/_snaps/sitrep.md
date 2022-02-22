ab3666dcc3a1b9a86989f5e8575984994ac17bc3
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-02-22 14:12:59 -0800

    Test with "darwin" (because that's what we send) and "windows"

--- tests/testthat/_snaps/sitrep.md
+++ tests/testthat/_snaps/sitrep.md
@@ -1,7 +1,7 @@
 # check_for_rstudio_updates
 
     Code
-      writeLines(check_for_rstudio_updates("mac", "0.0.1", TRUE))
+      writeLines(check_for_rstudio_updates("darwin", "0.0.1", TRUE))
     Output
       RStudio {VERSION} is now available (you're using 0.0.1).
       Download at: https://www.rstudio.com/products/rstudio/download/
@@ -9,7 +9,7 @@
 ---
 
     Code
-      writeLines(check_for_rstudio_updates("mac", "1.4.1717", TRUE))
+      writeLines(check_for_rstudio_updates("windows", "1.4.1717", TRUE))
     Output
       RStudio {VERSION} is now available (you're using 1.4.1717).
       Download at: https://www.rstudio.com/products/rstudio/download/
@@ -17,7 +17,7 @@
 ---
 
     Code
-      writeLines(check_for_rstudio_updates("mac", "2021.09.1+372", TRUE))
+      writeLines(check_for_rstudio_updates("darwin", "2021.09.1+372", TRUE))
     Output
       RStudio {VERSION} is now available (you're using 2021.09.1+372).
       Download at: https://www.rstudio.com/products/rstudio/download/
@@ -25,7 +25,7 @@
 ---
 
     Code
-      writeLines(check_for_rstudio_updates("mac", "2021.09.0-daily+328", TRUE))
+      writeLines(check_for_rstudio_updates("windows", "2021.09.0-daily+328", TRUE))
     Output
       RStudio {VERSION} is now available (you're using 2021.09.0-daily+328).
       Download at: https://www.rstudio.com/products/rstudio/download/
--- tests/testthat/test-sitrep.R
+++ tests/testthat/test-sitrep.R
@@ -2,11 +2,15 @@ test_that("check_for_rstudio_updates", {
   skip_if_offline()
   skip_on_cran()
 
+  # the IDE ends up calling this with `os = "mac"` on macOS, but we would send
+  # "darwin" in that case, so I test with "darwin"
+  # also mix in some "windows"
+
   # returns nothing rstudio not available
-  expect_null(check_for_rstudio_updates("mac", "1.0.0", FALSE))
+  expect_null(check_for_rstudio_updates("darwin", "1.0.0", FALSE))
 
   # returns nothing if the version is ahead of the current version
-  expect_null(check_for_rstudio_updates("mac", "2030.12.0+123", TRUE))
+  expect_null(check_for_rstudio_updates("windows", "2030.12.0+123", TRUE))
 
   # returns something if the version is behind the current version
   local_edition(3)
@@ -16,13 +20,13 @@ test_that("check_for_rstudio_updates", {
 
   # truly ancient
   expect_snapshot(
-    writeLines(check_for_rstudio_updates("mac", "0.0.1", TRUE)),
+    writeLines(check_for_rstudio_updates("darwin", "0.0.1", TRUE)),
     transform = scrub_current_version
   )
 
   # Juliet Rose, does not have long_version, last before numbering changed
   expect_snapshot(
-    writeLines(check_for_rstudio_updates("mac", "1.4.1717", TRUE)),
+    writeLines(check_for_rstudio_updates("windows", "1.4.1717", TRUE)),
     transform = scrub_current_version
   )
 
@@ -32,13 +36,13 @@ test_that("check_for_rstudio_updates", {
 
   # a superceded preview
   expect_snapshot(
-    writeLines(check_for_rstudio_updates("mac", "2021.09.1+372", TRUE)),
+    writeLines(check_for_rstudio_updates("darwin", "2021.09.1+372", TRUE)),
     transform = scrub_current_version
   )
 
   # a superceded daily
   expect_snapshot(
-    writeLines(check_for_rstudio_updates("mac", "2021.09.0-daily+328", TRUE)),
+    writeLines(check_for_rstudio_updates("windows", "2021.09.0-daily+328", TRUE)),
     transform = scrub_current_version
   )
 })
