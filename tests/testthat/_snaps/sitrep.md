5ca2cf63e512b84000b51512b048e67730f49d33
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-02-22 14:10:25 -0800

    Add tests

---
+++ tests/testthat/_snaps/sitrep.md
@@ -0,0 +1,32 @@
+# check_for_rstudio_updates
+
+    Code
+      writeLines(check_for_rstudio_updates("mac", "0.0.1", TRUE))
+    Output
+      RStudio {VERSION} is now available (you're using 0.0.1).
+      Download at: https://www.rstudio.com/products/rstudio/download/
+
+---
+
+    Code
+      writeLines(check_for_rstudio_updates("mac", "1.4.1717", TRUE))
+    Output
+      RStudio {VERSION} is now available (you're using 1.4.1717).
+      Download at: https://www.rstudio.com/products/rstudio/download/
+
+---
+
+    Code
+      writeLines(check_for_rstudio_updates("mac", "2021.09.1+372", TRUE))
+    Output
+      RStudio {VERSION} is now available (you're using 2021.09.1+372).
+      Download at: https://www.rstudio.com/products/rstudio/download/
+
+---
+
+    Code
+      writeLines(check_for_rstudio_updates("mac", "2021.09.0-daily+328", TRUE))
+    Output
+      RStudio {VERSION} is now available (you're using 2021.09.0-daily+328).
+      Download at: https://www.rstudio.com/products/rstudio/download/
+
--- tests/testthat/test-sitrep.R
+++ tests/testthat/test-sitrep.R
@@ -6,11 +6,39 @@ test_that("check_for_rstudio_updates", {
   expect_null(check_for_rstudio_updates("mac", "1.0.0", FALSE))
 
   # returns nothing if the version is ahead of the current version
-  # THIS NEEDS AN UPDATE
-  # expect_null(check_for_rstudio_updates("mac", "1000.0.0", TRUE))
+  expect_null(check_for_rstudio_updates("mac", "2030.12.0+123", TRUE))
 
   # returns something if the version is behind the current version
-  res <- check_for_rstudio_updates("mac", "0.0.1", TRUE)
-  expect_match(res, "RStudio .* is now available")
-  expect_match(res, "Download at")
+  local_edition(3)
+  scrub_current_version <- function(message) {
+    sub("(?<=^RStudio )[0-9\\.\\+]+", "{VERSION}", message, perl = TRUE)
+  }
+
+  # truly ancient
+  expect_snapshot(
+    writeLines(check_for_rstudio_updates("mac", "0.0.1", TRUE)),
+    transform = scrub_current_version
+  )
+
+  # Juliet Rose, does not have long_version, last before numbering changed
+  expect_snapshot(
+    writeLines(check_for_rstudio_updates("mac", "1.4.1717", TRUE)),
+    transform = scrub_current_version
+  )
+
+  # new scheme, introduced 2021-08
+  # YYYY.MM.<patch>[-(daily|preview)]+<build number>[.pro<pro suffix>]
+  # YYY.MM is th expected date of release for dailies and previews
+
+  # a superceded preview
+  expect_snapshot(
+    writeLines(check_for_rstudio_updates("mac", "2021.09.1+372", TRUE)),
+    transform = scrub_current_version
+  )
+
+  # a superceded daily
+  expect_snapshot(
+    writeLines(check_for_rstudio_updates("mac", "2021.09.0-daily+328", TRUE)),
+    transform = scrub_current_version
+  )
 })
