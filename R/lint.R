bb217060157d03d97ddf34579c0a403f8a5f9a42
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-05-16 10:49:59 -0700

    Delegate to the lintr docs for details like the cache location

--- R/lint.R
+++ R/lint.R
@@ -1,17 +1,15 @@
-#' Lint all source files in a package.
+#' Lint all source files in a package
 #'
 #' The default linters correspond to the style guide at
-#' <https://style.tidyverse.org/>, however it is possible to
-#' override any or all of them using the `linters` parameter.
+#' <https://style.tidyverse.org/>, however it is possible to override any or all
+#' of them using the `linters` parameter.
+#'
 #' @template devtools
-#' @param cache store the lint results so repeated lints of the same content
-#' use the previous results.
-#' @param ... additional arguments passed to [lintr::lint_package()]
+#' @param cache Store the lint results so repeated lints of the same content use
+#'   the previous results. Consult the lintr package to learn more about its
+#'   caching behaviour.
+#' @param ... Additional arguments passed to [lintr::lint_package()].
 #' @seealso [lintr::lint_package()], [lintr::lint()]
-#' @details
-#' The lintr cache is by default stored in `~/.R/lintr_cache/` (this can
-#' be configured by setting `options(lintr.cache_directory)`).  It can be
-#' cleared by calling [lintr::clear_cache()].
 #' @export
 lint <- function(pkg = ".", cache = TRUE, ...) {
   rlang::check_installed("lintr")
