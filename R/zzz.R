42fef7411f96234058db14f493dac1e87942a5ae
Hadley Wickham <h.wickham@gmail.com>
2022-05-23 11:59:03 -0500

    Move package doc to correct place

--- R/zzz.R
+++ R/zzz.R
@@ -5,41 +5,6 @@
 #' @import fs
 NULL
 
-#' Package development tools for R.
-#'
-#' @section Package options:
-#'
-#' Devtools uses the following [options()] to configure behaviour:
-#'
-#' \itemize{
-#'   \item `devtools.path`: path to use for [dev_mode()]
-#'
-#'   \item `devtools.name`: your name, used when signing draft
-#'     emails.
-#'
-#'   \item `devtools.install.args`: a string giving extra arguments passed
-#'     to `R CMD install` by [install()].
-#'
-#'   \item `devtools.desc.author`: a string providing a default Authors@@R
-#'     string to be used in new \file{DESCRIPTION}s.  Should be a R code, and
-#'     look like `"Hadley Wickham <h.wickham@@gmail.com> [aut, cre]"`. See
-#'     [utils::as.person()] for more details.
-#'
-#'   \item `devtools.desc.license`: a default license string to use for
-#'     new packages.
-#'
-#'   \item `devtools.desc.suggests`: a character vector listing packages to
-#'     to add to suggests by defaults for new packages.
-#
-#'   \item `devtools.desc`: a named list listing any other
-#'     extra options to add to \file{DESCRIPTION}
-#'
-#' }
-#' @docType package
-#' @keywords internal
-#' @name devtools
-"_PACKAGE"
-
 #' Deprecated Functions
 #'
 #' These functions are Deprecated in this release of devtools, they will be
