823ca2655e356d16df7645d6f1ef2b0024dc6d6f
Hadley Wickham <h.wickham@gmail.com>
2022-07-20 08:47:47 -0500

    Avoid R CMD check warning

--- tests/testthat/test-reload.R
+++ tests/testthat/test-reload.R
@@ -2,6 +2,7 @@ test_that("reload works", {
   withr::local_temp_libpaths()
 
   pkg <- as.package(test_path("testTest"))
+  pkg_name <- pkg$package
 
   install(pkg, quiet = TRUE)
   on.exit(unload(pkg$package), add = TRUE)
@@ -13,14 +14,14 @@ test_that("reload works", {
   expect_false(is_loaded(pkg))
 
   # Reload if loaded
-  requireNamespace(pkg$package, quietly = TRUE)
+  requireNamespace(pkg_name, quietly = TRUE)
   expect_true(is_loaded(pkg))
   reload(pkg, quiet = TRUE)
   expect_true(is_loaded(pkg))
 
   # Re-attach if attached
   unload(pkg$package, quiet = TRUE)
-  library(pkg$package, character.only = TRUE, quietly = TRUE)
+  library(pkg_name, character.only = TRUE, quietly = TRUE)
   expect_true(is_loaded(pkg))
   expect_true(is_attached(pkg))
   reload(pkg, quiet = TRUE)
