946e4a9daa70cd67ef32f41bb6bdde8fab47be6c
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-09 04:14:11 +0200

    Remove unused internal utilities (#2453)

--- R/utils.R
+++ R/utils.R
@@ -9,15 +9,6 @@ compact <- function(x) {
   get(f, envir = asNamespace(p))
 }
 
-# Modified version of utils::file_ext. Instead of always returning the text
-# after the last '.', as in "foo.tar.gz" => ".gz", if the text that directly
-# precedes the last '.' is ".tar", it will include also, so
-# "foo.tar.gz" => ".tar.gz"
-file_ext <- function(x) {
-  pos <- regexpr("\\.((tar\\.)?[[:alnum:]]+)$", x)
-  ifelse(pos > -1L, substring(x, pos + 1L), "")
-}
-
 is_windows <- isTRUE(.Platform$OS.type == "windows")
 
 sort_ci <- function(x) {
@@ -33,28 +24,6 @@ is_attached <- function(pkg = ".") {
   pkg <- as.package(pkg)
 
   !is.null(pkgload::pkg_env(pkg$package))
-}
-
-# This is base::trimws from 3.2 on
-trim_ws <- function(x, which = c("both", "left", "right")) {
-  which <- match.arg(which)
-  mysub <- function(re, x) sub(re, "", x, perl = TRUE)
-  if (which == "left") {
-    return(mysub("^[ \t\r\n]+", x))
-  }
-  if (which == "right") {
-    return(mysub("[ \t\r\n]+$", x))
-  }
-  mysub("[ \t\r\n]+$", mysub("^[ \t\r\n]+", x))
-}
-
-menu <- function(...) {
-  utils::menu(...)
-}
-
-escape_special_regex <- function(x) {
-  chars <- c("*", ".", "?", "^", "+", "$", "|", "(", ")", "[", "]", "{", "}", "\\")
-  gsub(paste0("([\\", paste0(collapse = "\\", chars), "])"), "\\\\\\1", x, perl = TRUE)
 }
 
 vcapply <- function(x, FUN, ...) {
