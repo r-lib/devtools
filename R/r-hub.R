45f24e156a4f8e4ef90ce278abd66401f1a2165b
Jakub Jędrusiak <kuba23031999@gmail.com>
2024-08-12 15:19:46 +0200

    follow rhub v2

--- R/r-hub.R
+++ R/r-hub.R
@@ -1,62 +1,45 @@
-
 #' Run CRAN checks for package on R-hub
 #'
-#' It runs [build()] on the package, with the arguments specified
-#' in `args`, and then submits it to the R-hub builder at
-#' <https://builder.r-hub.io>. The `interactive` option controls
-#' whether the function waits for the check output. Regardless, after the
-#' check is complete, R-hub sends an email with the results to the package
-#' maintainer.
+#' It runs an R-hub GitHub action on the package.
+#' Used to check if the package passes CRAN checks on
+#' different platforms and R versions.
 #'
-#' @section About email validation on r-hub:
-#' To build and check R packages on R-hub, you need to validate your
-#' email address. This is because R-hub sends out emails about build
-#' results. See more at [rhub::validate_email()].
+#' @section GitHub and R-hub:
+#' To use R-hub you must first setup a GitHub repository.
+#' Use [usethis::use_github()] to do that. Then setup your
+#' R-hub GH Action with [rhub::rhub_setup()].
 #'
-#' @param platforms R-hub platforms to run the check on. If `NULL`
-#'   uses default list of CRAN checkers (one for each major platform, and
-#'   one with extra checks if you have compiled code). You can also specify
-#'   your own, see [rhub::platforms()] for a complete list.
-#' @param email email address to notify, defaults to the maintainer
-#'   address in the package.
-#' @param interactive whether to show the status of the build
-#'   interactively. R-hub will send an email to the package maintainer's
-#'   email address, regardless of whether the check is interactive or not.
-#' @param build_args Arguments passed to `R CMD build`
-#' @param ... extra arguments, passed to [rhub::check_for_cran()].
-#' @inheritParams check
+#' @param platforms Platforms to use, a character vector.
+#' Defaults to most common platforms. Use NULL to select from
+#' a list in interactive sessions. See [rhub::rhub_platforms()].
+#'
+#' @inheritParams rhub::rhub_check
 #' @family build functions
-#' @return a `rhub_check` object.
+#' @return NULL
 #'
 #' @export
 
-check_rhub <- function(pkg = ".",
-                       platforms = NULL,
-                       email = NULL,
-                       interactive = TRUE,
-                       build_args = NULL,
-                       ...) {
+rhub_check <- function(gh_url = NULL,
+                       platforms = c("ubuntu-release", "windows", "macos"),
+                       r_versions = NULL,
+                       branch = NULL) {
   rlang::check_installed("rhub")
-  pkg <- as.package(pkg)
 
-  built_path <- build(pkg$path, tempdir(), quiet = !interactive,
-                      args = build_args)
-  on.exit(file_delete(built_path), add = TRUE)
+  rhub::rhub_doctor(gh_url = gh_url)
 
-  check_dots_used(action = getOption("devtools.ellipsis_action", rlang::warn))
+  rhub::rhub_check(gh_url = gh_url,
+                   platforms = platforms,
+                   r_versions = r_versions,
+                   branch = branch)
+}
 
-  status <- rhub::check_for_cran(
-    path = built_path,
-    email = email,
-    platforms = platforms,
-    show_status = interactive,
-    ...
-  )
+#' @rdname rhub_check
+#' @export
 
-  if (!interactive) {
-    cli::cli_inform(c(v = "R-hub check for package {.pkg {pkg$package}} submitted."))
-    status
-  } else {
-    status
-  }
+check_rhub <- function() {
+  lifecycle::deprecate_soft(when = "2.4.6", what = "check_rhub()", with = "rhub_check()")
+  rhub_check(gh_url = gh_url,
+             platforms = platforms,
+             r_versions = r_versions,
+             branch = branch)
 }
