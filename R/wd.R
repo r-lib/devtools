15f57c25a919a0eb630d61fa3294d5c9713f70ad
Hadley Wickham <h.wickham@gmail.com>
2026-01-22 08:54:07 -0600

    Deprecate outdated workflows
    Fixes #2427

--- R/wd.R
+++ R/wd.R
@@ -1,10 +1,17 @@
-#' Set working directory.
+#' Set working directory
+#'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' `wd()` is deprecated because we no longer use or recommend this workflow.
 #'
 #' @template devtools
 #' @param path path within package. Leave empty to change working directory
 #'   to package directory.
 #' @export
+#' @keywords internal
 wd <- function(pkg = ".", path = "") {
+  lifecycle::deprecate_warn("2.5.0", "wd()")
   pkg <- as.package(pkg)
   path <- path(pkg$path, path)
 
