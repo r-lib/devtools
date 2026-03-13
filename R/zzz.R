f702c33a8eaa68255815a443aed9fb7bd2231ba1
Jenny Bryan <jenny.f.bryan@gmail.com>
2026-03-13 14:44:50 -0700

    pak should never call sudo during `R CMD check`
    We can test and document devtools's use of pak without doing any sysreq stuff, which is what the `sudo` probe relates to.

--- R/zzz.R
+++ R/zzz.R
@@ -14,5 +14,12 @@ devtools_default_options <- list(
     options(devtools_default_options[toset])
   }
 
+  # Prevent pak from trying to install system requirements during R CMD check.
+  # On certain linux systems, it might call `sudo`, as a probe for capabilities.
+  # That is flagged by CRAN as problematic.
+  if (Sys.getenv("_R_CHECK_PACKAGE_NAME_", "") != "") {
+    options(pkg.sysreqs = FALSE)
+  }
+
   invisible()
 }
