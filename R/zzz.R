58d6e3b8d0f906c70f600d3b864856faf1176ea4
Jenny Bryan <jenny.f.bryan@gmail.com>
2026-03-13 15:19:24 -0700

    Work on the comment

--- R/zzz.R
+++ R/zzz.R
@@ -14,9 +14,12 @@ devtools_default_options <- list(
     options(devtools_default_options[toset])
   }
 
-  # Prevent pak from trying to install system requirements during R CMD check.
-  # On certain linux systems, it might call `sudo`, as a probe for capabilities.
-  # That is flagged by CRAN as problematic.
+  # On certain linux systems, pak might call `sudo`, as a probe for
+  # capabilities. That lays the ground work for a potential need to work with
+  # sysreqs, but that's not necessary in this case and CRAN flags the
+  # `sudo -s id` as problematic. Setting `pkg.sysreq` to `FALSE` prevents
+  # pak from even checking this.
+  # https://pak.r-lib.org/reference/pak-config.html#pak-configuration
   if (Sys.getenv("_R_CHECK_PACKAGE_NAME_", "") != "") {
     options(pkg.sysreqs = FALSE)
   }
